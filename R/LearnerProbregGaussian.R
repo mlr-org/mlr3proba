5770249f07e2c0701a0bc62448fdeea20192bda5
Raphael <raphael.sonabend.15@ucl.ac.uk>
2019-11-26 10:08:59 +0000

    Commented out density & probregr

--- R/LearnerProbregGaussian.R
+++ R/LearnerProbregGaussian.R
@@ -1,30 +1,30 @@
-LearnerProbregGaussian = R6::R6Class("LearnerProbregGaussian", inherit = LearnerProbreg)
-LearnerProbregGaussian$set("public", "initialize", function(id = "probregr.gaussian") {
-  super$initialize(
-    id = id,
-    predict_types = "prob",
-    feature_types = c("logical", "integer", "numeric", "character", "factor", "ordered"),
-    properties = c("missings", "importance", "selected_features"),
-    packages = "distr6"
-  )
-})
-LearnerProbregGaussian$set("public", "train_internal", function(task){
-  set_class(list(mean = mean(task$truth()), var = var(task$truth()), features = task$feature_names),
-            "probreg.Gaussian_model")
-})
-LearnerProbregGaussian$set("public", "predict_internal", function(task){
-  prob = VectorDistribution$new(distribution = Normal,
-                         params = data.table::data.table(mean = rep(self$model$mean, length(task$truth())),
-                                                         var = rep(self$model$var, length(task$truth()))))
-  PredictionProbreg$new(task = task, prob = prob)
-})
-LearnerProbregGaussian$set("public", "importance", function(){
-  if (is.null(self$model)) {
-    stopf("No model stored")
-  }
-  fn = self$model$features
-  named_vector(fn, 0)
-})
-LearnerProbregGaussian$set("public", "selected_features", function(){
-  character(0L)
-})
+# LearnerProbregGaussian = R6::R6Class("LearnerProbregGaussian", inherit = LearnerProbreg)
+# LearnerProbregGaussian$set("public", "initialize", function(id = "probregr.gaussian") {
+#   super$initialize(
+#     id = id,
+#     predict_types = "prob",
+#     feature_types = c("logical", "integer", "numeric", "character", "factor", "ordered"),
+#     properties = c("missings", "importance", "selected_features"),
+#     packages = "distr6"
+#   )
+# })
+# LearnerProbregGaussian$set("public", "train_internal", function(task){
+#   set_class(list(mean = mean(task$truth()), var = var(task$truth()), features = task$feature_names),
+#             "probreg.Gaussian_model")
+# })
+# LearnerProbregGaussian$set("public", "predict_internal", function(task){
+#   prob = VectorDistribution$new(distribution = Normal,
+#                          params = data.table::data.table(mean = rep(self$model$mean, length(task$truth())),
+#                                                          var = rep(self$model$var, length(task$truth()))))
+#   PredictionProbreg$new(task = task, prob = prob)
+# })
+# LearnerProbregGaussian$set("public", "importance", function(){
+#   if (is.null(self$model)) {
+#     stopf("No model stored")
+#   }
+#   fn = self$model$features
+#   named_vector(fn, 0)
+# })
+# LearnerProbregGaussian$set("public", "selected_features", function(){
+#   character(0L)
+# })
--- R/zzz.R
+++ R/zzz.R
@@ -19,47 +19,47 @@ register_mlr3 = function() {
     x = utils::getFromNamespace("mlr_reflections", ns = "mlr3")
     x$task_types = setkeyv(rbind(x$task_types, rowwise_table(
       ~type,     ~package,    ~task,         ~learner,         ~prediction,         ~measure,
-      "surv",    "mlr3proba", "TaskSurv",    "LearnerSurv",    "PredictionSurv",    "MeasureSurv",
-      "density", "mlr3proba", "TaskDensity", "LearnerDensity", "PredictionDensity", "MeasureDensity",
-      "probreg", "mlr3proba", "TaskProbreg", "LearnerProbreg", "PredictionProbreg", "MeasureDensity"
+      "surv",    "mlr3proba", "TaskSurv",    "LearnerSurv",    "PredictionSurv",    "MeasureSurv"
+      # "density", "mlr3proba", "TaskDensity", "LearnerDensity", "PredictionDensity", "MeasureDensity",
+      # "probreg", "mlr3proba", "TaskProbreg", "LearnerProbreg", "PredictionProbreg", "MeasureDensity"
     )), "type")
     x$task_col_roles$surv = c("feature", "target", "label", "order", "group", "weight")
-    x$task_col_roles$density = c("feature", "target", "label", "order", "group", "weight")
-    x$task_col_roles$probreg = c("feature", "target", "label", "order", "group", "weight")
+    # x$task_col_roles$density = c("feature", "target", "label", "order", "group", "weight")
+    # x$task_col_roles$probreg = c("feature", "target", "label", "order", "group", "weight")
     x$task_properties$surv = c("weights", "groups")
-    x$task_properties$density = c("weights", "groups")
-    x$task_properties$probreg = c("weights", "groups")
+    # x$task_properties$density = c("weights", "groups")
+    # x$task_properties$probreg = c("weights", "groups")
     x$learner_properties$surv = x$learner_properties$regr
-    x$learner_properties$density = x$learner_properties$regr
-    x$learner_properties$probreg = x$learner_properties$regr
+    # x$learner_properties$density = x$learner_properties$regr
+    # x$learner_properties$probreg = x$learner_properties$regr
     x$measure_properties$surv = x$measure_properties$regr
-    x$measure_properties$density = x$measure_properties$regr
-    x$measure_properties$probreg = x$measure_properties$regr
+    # x$measure_properties$density = x$measure_properties$regr
+    # x$measure_properties$probreg = x$measure_properties$regr
     x$learner_predict_types$surv = list(crank = c("crank","lp","distr"),
                                         distr = c("crank","lp","distr"),
                                         lp = c("crank","lp","distr"))
-    x$learner_predict_types$density = list(prob = "prob")
-    x$learner_predict_types$probreg = list(prob = "prob")
+    # x$learner_predict_types$density = list(prob = "prob")
+    # x$learner_predict_types$probreg = list(prob = "prob")
     x$default_measures$surv = "surv.harrellsc"
   }
 
   # tasks
    x = utils::getFromNamespace("mlr_tasks", ns = "mlr3")
-   x$add("precip", load_task_precip)
+   # x$add("precip", load_task_precip)
    x$add("rats", load_rats)
    x$add("lung", load_lung)
    x$add("unemployment", load_unemployment)
 
   # generators
    x = utils::getFromNamespace("mlr_task_generators", ns = "mlr3")
-   x$add("friedman1dens", TaskGeneratorFriedman1Dens)
+   # x$add("friedman1dens", TaskGeneratorFriedman1Dens)
    x$add("simsurv", TaskGeneratorSimsurv)
 
   # learners
    x = utils::getFromNamespace("mlr_learners", ns = "mlr3")
-   x$add("density.kde", LearnerDensityKDE)
+   # x$add("density.kde", LearnerDensityKDE)
 
-   x$add("probreg.gaussian", LearnerProbregGaussian)
+   # x$add("probreg.gaussian", LearnerProbregGaussian)
 
    x$add("surv.coxph", LearnerSurvCoxPH)
    x$add("surv.kaplan", LearnerSurvKaplanMeier)
@@ -80,8 +80,8 @@ register_mlr3 = function() {
 
   # measures
    x = utils::getFromNamespace("mlr_measures", ns = "mlr3")
-   x$add("density.logloss", MeasureDensityLogloss)
-   x$add("regr.logloss", MeasureRegrLogloss)
+   # x$add("density.logloss", MeasureDensityLogloss)
+   # x$add("regr.logloss", MeasureRegrLogloss)
    x$add("surv.brier", MeasureSurvBrier)
    x$add("surv.logloss", MeasureSurvLogloss)
    x$add("surv.logloss_se", MeasureSurvLoglossSE)
