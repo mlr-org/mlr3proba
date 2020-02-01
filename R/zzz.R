d2b7a835bc203bb2a401f8ff4a70df34c7d5c55d
Andreas Bender <bender.at.R@gmail.com>
2020-02-01 16:30:37 +0100

    [WIP] Add obliqueRSF learner

--- R/zzz.R
+++ R/zzz.R
@@ -74,6 +74,7 @@ register_mlr3 = function() {
    x$add("surv.parametric", LearnerSurvParametric)
    x$add("surv.flexible", LearnerSurvFlexible)
    x$add("surv.gbm", LearnerSurvGBM)
+   x$add("surv.obliqueRSF", LearnerSurvObliqueRSF)
 
   # measures
    x = utils::getFromNamespace("mlr_measures", ns = "mlr3")
---
+++ tests/testthat/test_mlr_learners_surv_obliuqersf.R
@@ -0,0 +1,10 @@
+context("surv.obliqueRSF")
+
+test_that("autotest", {
+
+  learner = mlr_learners$get("surv.obliqueRSF")
+  expect_learner(learner)
+  result = run_autotest(learner)
+  expect_true(result, info = result$error)
+
+})
