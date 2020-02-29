2256d344db14e228e23c8b19ede6bf7e22abc72e
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-02-29 17:22:19 +0000

    add kdesm

---
+++ tests/testthat/test_mlr_learners_density_sm.R
@@ -0,0 +1,9 @@
+context("dens.kdeSM")
+
+test_that("autotest", {
+  set.seed(1)
+  learner = lrn("dens.kdeSM")
+  expect_learner(learner)
+  result = run_autotest(learner)
+  expect_true(result, info = result$error)
+})
