e7bc704840e76bb2a770c7cf61b3cad725a25785
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-02-29 12:23:15 +0000

    tests

---
+++ tests/testthat/test_mlr_measures_dens.R
@@ -0,0 +1,17 @@
+context("mlr_measures_dens")
+
+task = TaskGeneratorSimdens$new()$generate(20)
+
+test_that("mlr_measures", {
+  keys = mlr_measures$keys("^dens")
+
+  for (key in keys) {
+
+    m = mlr_measures$get(key)
+
+    expect_measure(m)
+
+    perf = mlr_learners$get("dens.hist")$train(task)$predict(task)$score()
+    expect_number(perf, na.ok = "na_score" %in% m$properties)
+  }
+})
---
+++ tests/testthat/test_mlr_task_generators_simdens.R
@@ -0,0 +1,7 @@
+context("mlr_task_generators_simdens")
+
+test_that("mlr_task_generators_simdens", {
+  gen = mlr_task_generators$get("simdens")
+  task = gen$generate(10)
+  expect_task_dens(task)
+})
