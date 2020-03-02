0a9e87c245c42ffa8a952af2cbc07c194b21fec3
Ain Toha <nurul.toha.15@ucl.ac.uk>
2020-03-02 06:19:22 +0000

    edit
    1. change name
    2. remove comments
    3. remove default bandwidth

--- R/LearnerDensKDE.R
+++ R/LearnerDensKDE.R
@@ -11,9 +11,9 @@ LearnerDensKDE$set("public", "initialize", function(id = "dens.kde") {
                                                       select="ShortName")[[1]],
                                       default = "Norm",
                                       tags = "train"),
-                         ParamDbl$new("bandwidth", lower = 0, tags = "train", default = 1)))
+                         ParamDbl$new("bandwidth", lower = 0, tags = "train")))
 
-  ps$values = list(kernel = "Norm", bandwidth = 1)
+  ps$values = list(kernel = "Norm")
 
   super$initialize(
     id = id,
@@ -26,25 +26,24 @@ LearnerDensKDE$set("public", "initialize", function(id = "dens.kde") {
 })
 LearnerDensKDE$set("public", "train_internal", function(task){
   pdf <- function(x1){}
-  #x1 is new data
+
   body(pdf) <- substitute({
-    # x1 <- matrix(x1, nrow = length(x1), ncol = rows)
+
     ntrain = as.numeric(train)
-    # truth <- matrix(truth, nrow = nrow(x1), ncol = rows, byrow = TRUE)
-    # if(nrow(x1) == 1)
+
     return(1/(rows * bw) * sum(kernel$pdf((x1 - ntrain)/bw)))
-    # else
-    # return(1/(rows * bw) * colSums(apply((x1 - tru_mat)/bw,1,kernel$pdf)))
+
 
   }, list(rows = task$nrow,
           bw = self$param_set$values$bandwidth,
           kernel = get(as.character(subset(listKernels(),
-                                           ShortName == self$param_set$values$kernel,
-                                           ClassName)))$new(),
+          ShortName == self$param_set$values$kernel,
+          ClassName)))$new(),
           train = task$truth()))
 
-  Distribution$new(name = paste("KDE", self$param_set$values$kernel),
-                   short_name = paste0("KDE",self$param_set$values$kernel), pdf = pdf)
+  Distribution$new(name = paste(self$param_set$values$kernel, "KDE"),
+                   short_name = paste0(self$param_set$values$kernel, "_KDE"),
+                   pdf = pdf)
 
 })
 LearnerDensKDE$set("public", "predict_internal", function(task){
