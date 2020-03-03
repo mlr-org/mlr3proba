1ca4a6371548d4c35177b9158ef9ea584781af66
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-03-03 13:49:09 +0000

    docs, quantile

--- R/LearnerDensSpline.R
+++ R/LearnerDensSpline.R
@@ -1,6 +1,6 @@
 #' @template dens_learner
 #' @templateVar title Smoothing Splines
-#' @templateVar fullname LearnerDensPenGss
+#' @templateVar fullname LearnerDensSpline
 #' @templateVar caller [gss::ssden()]
 #'
 #' @export
@@ -11,21 +11,21 @@ LearnerDensSpline <- R6::R6Class("LearnerDensSpline", inherit = LearnerDens,
       param_set = ParamSet$new(
         params = list(
           ParamUty$new(id = "type", tags = "train"),
-          ParamUty$new(id = "data", tags = "train"),
-          ParamDbl$new(id = "alpha", default =1.4, tags = "train"),
+          ParamDbl$new(id = "alpha", default = 1.4, tags = "train"),
           ParamUty$new(id = "weights", tags = "train"),
-          ParamUty$new(id = "na.action", default =na.omit, tags = "train"),
+          ParamUty$new(id = "na.action", default = na.omit, tags = "train"),
           ParamDbl$new(id = "nbasis",  tags = "train"),
           ParamDbl$new(id = "seed",  tags = "train"),
           ParamUty$new(id = "domain", tags = "train"),
           ParamUty$new(id = "quad", tags = "train"),
-          ParamUty$new(id = "qdsz.depth",  tags = "train"),
+          ParamDbl$new(id = "qdsz.depth",  tags = "train"),
           ParamUty$new(id = "bias", tags = "train"),
           ParamDbl$new(id = "prec", default = 1e-7, tags = "train"),
-          ParamDbl$new(id = "maxiter", default = 30, tags = "train"),
+          ParamInt$new(id = "maxiter", default = 30, lower = 1, tags = "train"),
           ParamLgl$new(id = "skip.iter",  tags = "train")
-         )),
+        )),
       feature_types =  c("logical", "integer", "numeric", "character", "factor", "ordered"),
+      properties = "missings",
       predict_types = c("pdf", "cdf"),
       packages = c("gss", "distr6")
     )},
@@ -34,14 +34,14 @@ LearnerDensSpline <- R6::R6Class("LearnerDensSpline", inherit = LearnerDens,
 
       pars = self$param_set$get_values(tags = "train")
 
-      data = as.numeric(unlist(task$data(cols = task$target_names)))
+      data = task$truth()
 
       fit = invoke(gss::ssden, formula = ~data, .args = pars)
 
       pdf <- function(x1){}
       body(pdf) <- substitute({
 
-      invoke(gss::dssden, object = fit, x = x1)
+        invoke(gss::dssden, object = fit, x = x1)
 
       })
 
@@ -55,26 +55,20 @@ LearnerDensSpline <- R6::R6Class("LearnerDensSpline", inherit = LearnerDens,
       quantile <- function(x1){}
       body(quantile) <- substitute({
 
-        invoke(gss::qssden, object = fit, p= x1)
+        invoke(gss::qssden, object = fit, p = x1)
 
       })
 
 
-      Distribution$new(name = "gss Penalized Density",
-                       short_name = "gssPenDens",
-                       pdf = pdf, cdf = cdf)#, quantile = quantile)
-
-
-
+      Distribution$new(name = "Smoothing Spline Density Estimator",
+                       short_name = "splineDens",
+                       pdf = pdf, cdf = cdf, quantile = quantile)
     },
 
     predict_internal = function(task){
-
-      newdata = as.numeric(unlist(task$data(cols = task$target_names)))
+      newdata = task$truth()
 
       PredictionDens$new(task = task, pdf = self$model$pdf(newdata),
                          cdf = self$model$cdf(newdata))
-                        # quantile = self$model$quantile(newdata))
-
     }
   ))
