10a7db3c2f2f2ba3cfec13ea699adccbce7d10f8
Ain Toha <nurul.toha.15@ucl.ac.uk>
2020-03-03 11:10:52 +0000

    Update test_mlr_learners_density_kde.R
    Error: mismatch

--- tests/testthat/test_mlr_learners_density_kde.R
+++ tests/testthat/test_mlr_learners_density_kde.R
@@ -7,3 +7,23 @@ test_that("autotest", {
   result = run_autotest(learner)
   expect_true(result, info = result$error)
 })
+
+
+data = data.frame("A" = c(0.2, 0.4, 0.6, 0.8, 1, 1.2, 1.4, 1.6))
+a <- ks::kde(x = data, h = 0.1,  eval.points = c(0, 0.5, 1, 1.5, 2, -1, 3))$estimate
+a
+task = TaskDens$new(id = "a", data, target = "A")
+lrn = lrn("dens.kde", bandwidth = 0.1, kernel = "Norm")
+p = lrn$train(task)
+dist = p$model
+
+test_that("pdf", {
+  expect_equal(dist$pdf(0.5), 0.6160504979)
+  # expect_equal(dist$pdf(0.5), 0.5)
+  # expect_equal(dist$pdf(1.0), 0.75)
+  # expect_equal(dist$pdf(1.5), 0.25)
+  # expect_equal(dist$pdf(2.0), 0.25)
+  # expect_equal(dist$pdf(-1), 0)
+  # expect_equal(dist$pdf(3), 0)
+})
+
