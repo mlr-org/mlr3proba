4b9eebe5a0eab3c15060bc72f9a46f0477906a36
Ain Toha <nurul.toha.15@ucl.ac.uk>
2020-03-03 14:26:34 +0000

    Delete DensGenKern.R

--- R/DensGenKern.R
+++
@@ -1,4 +0,0 @@
-.DensGenKern <- function(x, xgridsize = 100, range.x, xbandwidth = 1){
-    return(sapply(range.x, function(y) GenKern::KernSec(x = x, xgridsize = xgridsize, xbandwidth = xbandwidth,
-                                            range.x = y)$yden/xgridsize))
-}
