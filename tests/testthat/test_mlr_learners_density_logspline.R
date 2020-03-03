923e561931964589cd6ffdad2777808b189d74e8
Ain Toha <nurul.toha.15@ucl.ac.uk>
2020-03-03 21:08:58 +0000

    Update and rename test_mlr_learners_density_spline.R to test_mlr_learners_density_logspline.R

--- tests/testthat/test_mlr_learners_density_logspline.R
+++ tests/testthat/test_mlr_learners_density_logspline.R
@@ -1,8 +1,8 @@
-context("dens.spline")
+context("dens.logspline")
 
 test_that("autotest", {
   set.seed(1)
-  learner = lrn("dens.spline")
+  learner = lrn("dens.logspline")
   expect_learner(learner)
   result = run_autotest(learner)
   expect_true(result, info = result$error)
