6f57c697a062f3495e2bc9b8f871f59db30130a6
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-03-03 15:53:01 +0000

    Update LearnerDensKDEgk.R

--- R/LearnerDensKDEgk.R
+++ R/LearnerDensKDEgk.R
@@ -4,44 +4,46 @@
 #' @templateVar caller [GenKern::KernSec]
 #'
 #' @export
-LearnerDensKDEgk = R6::R6Class("LearnerDensKDEgk", inherit = LearnerDens)
-LearnerDensKDEgk$set("public", "initialize", function(id = "dens.kdeGK") {
-  ps = ParamSet$new(list(ParamUty$new(id = "xbandwidth", tags = "train"),
-                         ParamInt$new(id = "xgridsize", lower =0, default = 100, tags = "train")
-  ))
+LearnerDensKDEgk = R6::R6Class("LearnerDensKDEgk", inherit = LearnerDens,
+  public = list(
+    initialize = function(id = "dens.kdeGK") {
+      super$initialize(
+        id = id,
+        param_set = ParamSet$new(
+          list(
+            ParamUty$new(id = "xbandwidth", tags = "train"),
+            ParamInt$new(id = "xgridsize", lower =0, default = 100, tags = "train")
+          )
+        ),
+        predict_types = "pdf",
+        feature_types = c("logical", "integer", "numeric", "character", "factor", "ordered"),
+        properties = "missings",
+        packages = c("GenKern", "KernSmooth", "distr6")
+      )
+    },
 
-  ps$values = list(xgridsize = 100)
+    train_internal = function(task){
+      pars = self$param_set$get_values(tag = "train")
+      pdf <- function(x1){}
+      body(pdf) <- substitute({
+        with_package("KernSmooth",{
+          return(sapply(x1, function(y) invoke(GenKern::KernSec,
+                                               x = train,
+                                               range.x = y,
+                                               .args = pars)$yden/rows))
+        })
+      }, list(rows = task$nrow,
+              pars = pars,
+              train = task$truth()))
 
-  super$initialize(
-    id = id,
-    param_set = ps,
-    predict_types = "pdf",
-    feature_types = c("logical", "integer", "numeric", "character", "factor", "ordered"),
-    properties = "missings",
-    packages = c("GenKern", "distr6")
+      Distribution$new(name = "GenKern KDE",
+                       short_name = "GenKernKDE",
+                       pdf = pdf)
+
+    },
+
+    predict_internal = function(task){
+      PredictionDens$new(task = task, pdf = self$model$pdf(task$truth()))
+    }
   )
-})
-LearnerDensKDEgk$set("public", "train_internal", function(task){
-  pdf <- function(x1){}
-
-  body(pdf) <- substitute({
-
-    ntrain = as.numeric(train)
-
-    return(sapply(x1, function(y) GenKern::KernSec(x = ntrain, xbandwidth = xb,
-                                                   xgridsize = xg, range.x = y)$yden/rows))
-
-
-  }, list(rows = task$nrow,
-          xb = self$param_set$values$xbandwidth,
-          xg = self$param_set$values$xgridsize,
-          train = task$truth()))
-
-  Distribution$new(name = "GenKern KDE",
-                   short_name = "GenKernKDE",
-                   pdf = pdf)
-
-})
-LearnerDensKDEgk$set("public", "predict_internal", function(task){
-  PredictionDens$new(task = task, pdf = self$model$pdf(task$truth()))
-})
+)
