f73062185c8c129f862c42d704cbaed62b20b219
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-03-03 14:23:12 +0000

    Update LearnerDensKDEks.R

--- R/LearnerDensKDEks.R
+++ R/LearnerDensKDEks.R
@@ -6,58 +6,54 @@
 #' @export
 LearnerDensKDEks <- R6::R6Class("LearnerDensKDEks", inherit = LearnerDens,
   public = list(initialize = function(id = "dens.kdeKS"){
-          super$initialize(
-                  id = id,
-                  param_set = ParamSet$new(
-                          params = list(
-                                  ParamDbl$new(id = "h", lower = 0,   tags = "train"),
-                                  ParamUty$new(id = "H", tags = "train"),
-                                  ParamUty$new(id = "gridtype", tags = "train"),
-                                  ParamDbl$new(id = "xmin", tags = "train"),
-                                  ParamDbl$new(id = "xmax", tags = "train"),
-                                  ParamDbl$new(id = "supp", default =3.7, tags= "train"),
-                                  ParamDbl$new(id = "binned", tags = "train"),
-                                  ParamUty$new(id = "bgridsize", tags = "train"),
-                                  ParamLgl$new(id = "positive", default = FALSE, tags = "train"),
-                                  ParamUty$new(id = "adj.positive", tags = "train"),
-                                  ParamUty$new(id = "w", tags = "train"),
-                                  ParamLgl$new(id = "compute.cont", default =TRUE, tags= "train"),
-                                  ParamLgl$new(id = "approx.cont", default =TRUE, tags = "train"),
-                                  ParamLgl$new(id = "unit.interval",  default=FALSE, tags = "train"),
-                                  ParamLgl$new(id = "zero.flag", tags = "train")
+    super$initialize(
+      id = id,
+      param_set = ParamSet$new(
+        params = list(
+          ParamDbl$new(id = "h", lower = 0,   tags = "train"),
+          ParamUty$new(id = "H", tags = "train"),
+          ParamUty$new(id = "gridtype", tags = "train"),
+          ParamDbl$new(id = "xmin", tags = "train"),
+          ParamDbl$new(id = "xmax", tags = "train"),
+          ParamDbl$new(id = "supp", default =3.7, tags= "train"),
+          ParamDbl$new(id = "binned", tags = "train"),
+          ParamUty$new(id = "bgridsize", tags = "train"),
+          ParamLgl$new(id = "positive", default = FALSE, tags = "train"),
+          ParamUty$new(id = "adj.positive", tags = "train"),
+          ParamUty$new(id = "w", tags = "train"),
+          ParamLgl$new(id = "compute.cont", default =TRUE, tags= "train"),
+          ParamLgl$new(id = "approx.cont", default =TRUE, tags = "train"),
+          ParamLgl$new(id = "unit.interval",  default=FALSE, tags = "train"),
+          ParamLgl$new(id = "zero.flag", tags = "train")
 
-                          )),
-                  feature_types =  c("logical", "integer", "numeric", "character", "factor", "ordered"),
-                  predict_types = "pdf",
-                  packages = c("ks", "distr6")
-          )},
+        )),
+      feature_types =  c("logical", "integer", "numeric", "character", "factor", "ordered"),
+      predict_types = "pdf",
+      packages = c("ks", "distr6")
+    )},
 
-          train_internal = function(task){
+    train_internal = function(task){
 
-                  pars = self$param_set$get_values(tag="train")
+      pars = self$param_set$get_values(tag="train")
 
-                  data = as.numeric(unlist(task$data(cols = task$target_names)))
+      data = task$truth()
 
-                  pdf <- function(x1){}
+      pdf <- function(x1){}
 
-                  body(pdf) <- substitute({
+      body(pdf) <- substitute({
 
-                          invoke(ks::kde, x = data, eval.points = x1, .args =pars)$estimate
+        invoke(ks::kde, x = data, eval.points = x1, .args =pars)$estimate
 
-                  })
+      })
 
-                  Distribution$new(name = "ks KDE",
-                                   short_name = "ksKDE",
-                                   pdf = pdf)
-          },
+      Distribution$new(name = "ks KDE",
+                       short_name = "ksKDE",
+                       pdf = pdf)
+    },
 
-          predict_internal = function(task){
-
-                  newdata = as.numeric(unlist(task$data(cols = task$target_names)))
-
-                  PredictionDens$new(task = task, pdf = self$model$pdf(newdata))
-
-          }
+    predict_internal = function(task){
+      PredictionDens$new(task = task, pdf = self$model$pdf(task$truth()))
+    }
   )
- )
+)
 
