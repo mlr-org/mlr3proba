b151271423e4121c23ae537cc59a31e674a30ffb
Ain Toha <nurul.toha.15@ucl.ac.uk>
2020-03-03 14:47:01 +0000

    edit locfit

--- R/LearnerDensLogLocfit.R
+++ R/LearnerDensLogLocfit.R
@@ -4,53 +4,56 @@
 #' @templateVar caller [locfit::density.lf()]
 #'
 #' @export
-LearnerDensLogLocfit <- R6::R6Class("LearnerDensLogLocfit", inherit = LearnerDens,
-  public = list(initialize = function(id = "dens.logLF"){
-    super$initialize(
-      id = id,
-      param_set = ParamSet$new(
-        params = list(
-          ParamFct$new(id = "window", levels = c("gaussian", "epanechnikov", "rectangular",
-                                                 "triangular", "biweight", "uniform",
-                                                 "optcosine"),
-                       default = "gaussian", tags = "train"),
-          ParamDbl$new(id = "width", tags = "train"),
-          ParamDbl$new(id = "from", tags = "train"),
-          ParamDbl$new(id = "to", tags = "train"),
-          ParamUty$new(id = "cut", default = 0L, tags = "train"),
-          ParamDbl$new(id = "deg", default = 0, tags = "train"),
-          ParamUty$new(id = "family", default = "density", tags = "train"),
-          ParamUty$new(id = "link", default = "ident", tags = "train")
-        )),
-      feature_types =  c("logical", "integer", "numeric", "character", "factor", "ordered"),
-      predict_types = "pdf",
-      packages = c("locfit", "distr6")
-    )},
+LearnerDensLocfit <- R6::R6Class("LearnerDensLocfit", inherit = LearnerDens,
+  public = list(initialize = function(id = "dens.locfit"){
+      ps = ParamSet$new(
+           params = list(
+           ParamFct$new(id = "window", levels = c("gaussian", "epanechnikov", "rectangular",
+                                               "triangular", "biweight", "uniform",
+                                               "optcosine"), default = "gaussian", tags = "train"),
+           ParamDbl$new(id = "width", tags = "train"),
+           ParamDbl$new(id = "from", tags = "train"),
+           ParamDbl$new(id = "to", tags = "train"),
+           ParamUty$new(id = "cut",  tags = "train"),
+           ParamDbl$new(id = "deg", default = 0, tags = "train"),
+           ParamUty$new(id = "family", default = "density", tags = "train"),
+           ParamUty$new(id = "link", default = "identity", tags = "train")
+          ))
 
-    train_internal = function(task){
+          ps$values = list(window = "gaussian", family = "density", link = "identity")
 
-      pars = self$param_set$get_values(tag="train")
+          super$initialize(
+          id = id,
+          param_set = ps,
+          feature_types =  c("logical", "integer", "numeric", "character", "factor", "ordered"),
+          predict_types = "pdf",
+          packages = c("locfit", "distr6")
+          )},
 
-      data = as.numeric(unlist(task$data(cols = task$target_names)))
+          train_internal = function(task){
 
-      pdf <- function(x1){}
+          pars = self$param_set$get_values(tag="train")
 
-      body(pdf) <- substitute({
+          data = task$target()
 
-        invoke(locfit::density.lf, x = data, ev = x1, .args = pars)$y
+          pdf <- function(x1){}
 
-      })
+          body(pdf) <- substitute({
 
-      Distribution$new(name = paste("LocFit Density", self$param_set$values$window),
+          invoke(locfit::density.lf, x = data, ev = x1, .args = pars)$y
+
+          })
+
+          Distribution$new(name = paste("LocFit Density", self$param_set$values$window),
                        short_name = paste0("LocFitDens",self$param_set$values$window),
                        pdf = pdf)
-    },
+          },
 
-    predict_internal = function(task){
+        predict_internal = function(task){
 
-      newdata = as.numeric(unlist(task$data(cols = task$target_names)))
+        newdata = task$target()
 
-      PredictionDens$new(task = task, pdf = self$model$pdf(newdata))
+        PredictionDens$new(task = task, pdf = self$model$pdf(newdata))
 
     }
 
--- R/zzz.R
+++ R/zzz.R
@@ -64,7 +64,7 @@ register_mlr3 = function() {
 
    x$add("dens.hist", LearnerDensHistogram)
    x$add("dens.kde", LearnerDensHistogram)
-   x$add("dens.logLF", LearnerDensLogLocfit)
+   x$add("dens.locfit", LearnerDensLocfit)
 
    x$add("surv.coxph", LearnerSurvCoxPH)
    x$add("surv.kaplan", LearnerSurvKaplan)
