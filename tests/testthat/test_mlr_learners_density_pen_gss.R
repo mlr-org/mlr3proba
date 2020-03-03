c9d132e55cceb57b8b18ffa7eb19ff9a67031bf6
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-03-03 13:55:47 +0000

    Delete test_mlr_learners_density_pen_gss.R

--- tests/testthat/test_mlr_learners_density_pen_gss.R
+++
@@ -1,9 +0,0 @@
-context("dens.penGSS")
-
-test_that("autotest", {
-  set.seed(1)
-  learner = lrn("dens.penGSS")
-  expect_learner(learner)
-  result = run_autotest(learner)
-  expect_true(result, info = result$error)
-})
