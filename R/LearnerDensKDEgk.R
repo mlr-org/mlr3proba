972f83476a10760e5a946e59b5681fb39576a256
Ain Toha <nurul.toha.15@ucl.ac.uk>
2020-03-03 14:25:18 +0000

    edit LearnerDenKDEgk

--- R/LearnerDensKDEgk.R
+++ R/LearnerDensKDEgk.R
@@ -1,49 +1,41 @@
-#' @template dens_learner
-#' @templateVar title Gaussian Univariate Kernel
-#' @templateVar fullname LearnerDensKDEgk
-#' @templateVar caller [GenKern::KernSec()]
-#'
-#' @export
-LearnerDensKDEgk <- R6::R6Class("LearnerDensKDEgk", inherit = LearnerDens,
-  public = list(initialize = function(id = "dens.kdeGK"){
-    super$initialize(
-      id = id,
-      param_set = ParamSet$new(
-        params = list(
-          ParamDbl$new(id = "xbandwidth", default = 1,  tags = "train"),
-          ParamDbl$new(id = "xgridsize", default = 100, tags = "train")
-        )),
-      feature_types =  c("integer", "numeric", "character", "factor", "ordered"),
-      predict_types = "pdf",
-      packages = c("GenKern", "distr6")
-    )},
+LearnerDensKDEgk = R6::R6Class("LearnerDensKDEgk", inherit = LearnerDens)
+LearnerDensKDEgk$set("public", "initialize", function(id = "dens.kdeGK") {
+  ps = ParamSet$new(list(ParamUty$new(id = "xbandwidth", tags = "train"),
+                         ParamInt$new(id = "xgridsize", default = 100, tags = "train")
+  ))
 
-    train_internal = function(task){
+  ps$values = list(xgridsize = 100)
 
-      pars = self$param_set$get_values(tag="train")
-      data = as.numeric(unlist(task$data(cols = task$target_names)))
-
-      pdf = function(x1){}
-      body(pdf) = substitute({
-
-        invoke(.DensGenKern, x = data, range.x = x1, .args = pars)
-
-      })
-
-      Distribution$new(name = "GenKern KDE Gaussian",
-                       short_name = "GenKernKDEGaus",
-                       pdf = pdf)
-
-
-    },
-
-    predict_internal = function(task){
-
-      newdata = as.numeric(unlist(task$data(cols = task$target_names)))
-
-      PredictionDens$new(task = task, pdf = self$model$pdf(newdata))
-
-    }
+  super$initialize(
+    id = id,
+    param_set = ps,
+    predict_types = "pdf",
+    feature_types = c("logical", "integer", "numeric", "character", "factor", "ordered"),
+    properties = "missings",
+    packages = c("GenKern", "distr6")
   )
-)
+})
+LearnerDensKDEgk$set("public", "train_internal", function(task){
+  pdf <- function(x1){}
 
+  body(pdf) <- substitute({
+
+    ntrain = as.numeric(train)
+
+    return(sapply(x1, function(y) GenKern::KernSec(x = ntrain, xbandwidth = xb,
+                                                   xgridsize = xg, range.x = y)$yden/rows))
+
+
+  }, list(rows = task$nrow,
+          xb = self$param_set$values$xbandwidth,
+          xg = self$param_set$values$xgridsize,
+          train = task$truth()))
+
+  Distribution$new(name = "GenKern KDE Gaussian",
+                   short_name = "GenKernKDEGaus",
+                   pdf = pdf)
+
+})
+LearnerDensKDEgk$set("public", "predict_internal", function(task){
+  PredictionDens$new(task = task, pdf = self$model$pdf(task$truth()))
+})
