f9e0c8346b6e067dfc0e6ef402bc2dfbbd13b6a9
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-03-04 13:32:44 +0000

    update dens sanity helper

--- tests/testthat/test_mlr_learners_density_logspline.R
+++ tests/testthat/test_mlr_learners_density_logspline.R
@@ -4,6 +4,6 @@ test_that("autotest", {
   set.seed(1)
   learner = lrn("dens.logspline")
   expect_learner(learner)
-  result = run_autotest(learner, N= 1000)
+  result = run_autotest(learner)
   expect_true(result, info = result$error)
 })
