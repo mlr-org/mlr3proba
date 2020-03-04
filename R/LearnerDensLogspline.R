7910542ede3ef0bb3cd92b926a8fcaf8c0a2ae95
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-03-04 13:42:06 +0000

    Update LearnerDensLogspline.R

--- R/LearnerDensLogspline.R
+++ R/LearnerDensLogspline.R
@@ -9,15 +9,14 @@ LearnerDensLogspline<- R6::R6Class("LearnerDensLogspline", inherit = LearnerDens
     super$initialize(
       id = id,
       param_set = ParamSet$new(list(
-        ParamDbl$new(id = "maxknots", default = 0,  tags = "train"),
-        ParamUty$new(id = "penalty", tags = "train"),
         ParamDbl$new(id = "lbound", tags = "train"),
         ParamDbl$new(id = "ubound", tags = "train"),
+        ParamDbl$new(id = "maxknots", default = 0,  lower = 0, tags = "train"),
         ParamUty$new(id = "knots", tags = "train"),
-        ParamDbl$new(id = "nknots", default = 0, tags = "train"),
+        ParamDbl$new(id = "nknots", default = 0, lower = 0, tags = "train"),
+        ParamUty$new(id = "penalty", tags = "train"),
         ParamDbl$new(id = "mind", default = -1, tags ="train"),
-        ParamLgl$new(id = "silent", default = TRUE, tags = "train"),
-        ParamUty$new(id = "error.action", default = 2, tags = "train")
+        ParamInt$new(id = "error.action", default = 2, lower = 0, upper = 2, tags = "train")
       )),
       feature_types =  c("logical", "integer", "numeric", "character", "factor", "ordered"),
       predict_types = c("pdf", "cdf"),
@@ -33,51 +32,34 @@ LearnerDensLogspline<- R6::R6Class("LearnerDensLogspline", inherit = LearnerDens
       fit = invoke(logspline::logspline, x = data, .args = pars)
 
       pdf <- function(x1){}
-
       body(pdf) <- substitute({
-
        invoke(logspline::dlogspline, q = x1, fit = fit)
-
       })
 
-
       cdf <- function(x1){}
-
       body(cdf) <- substitute({
-
         invoke(logspline::plogspline, q = x1, fit = fit)
-
       })
 
       quantile <- function(x1){}
-
       body(quantile) <- substitute({
-
-        invoke(logspline::qlogspline, q = x1, fit = fit)
-
+        invoke(logspline::qlogspline, p = x1, fit = fit)
       })
 
       rand <- function(x1){}
-
       body(rand) <- substitute({
-
         invoke(logspline::rlogspline, n = x1, fit = fit)
-
       })
 
-
-      Distribution$new(name = "Logspline Density Penalized",
+      Distribution$new(name = "Logspline Density Estimator",
                        short_name = "LogsplineDens",
                        pdf = pdf, cdf = cdf, quantile = quantile, rand = rand)
 
     },
 
     predict_internal = function(task){
-
-      newdata = task$truth()
-
-      PredictionDens$new(task = task, pdf = self$model$pdf(newdata),
-                         cdf = self$model$cdf(newdata))
-
+      PredictionDens$new(task = task,
+                         pdf = self$model$pdf(task$truth()),
+                         cdf = self$model$cdf(task$truth()))
     }
   ))
