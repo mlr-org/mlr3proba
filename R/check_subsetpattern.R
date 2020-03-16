18acc0226089df1491f851be7d7a4f11292ca0c9
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-03-16 15:56:53 +0000

    cleanup

--- R/check_subsetpattern.R
+++
@@ -1,8 +0,0 @@
-check_subsetpattern = function(x, choices, empty.ok = TRUE){
-  if(all(grepl(paste0(choices, collapse="|"), x)))
-    return(TRUE)
-  else
-    return(sprintf("Must be a subset of %s, but is %s",
-                   paste0("{", paste0(choices, collapse = ", "), "}"),
-                   paste0("{", paste0(x, collapse = ", "), "}")))
-}
