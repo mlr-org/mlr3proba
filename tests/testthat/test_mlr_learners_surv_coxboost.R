fb4fc4872e5d6efa52cd79334e68ec8f5b4449b5
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-04-06 13:00:37 +0100

    removed tests

--- tests/testthat/test_mlr_learners_surv_coxboost.R
+++
@@ -1,8 +0,0 @@
-context("surv.coxboost")
-
-test_that("autotest", {
-  learner = mlr_learners$get("surv.coxboost")
-  expect_learner(learner)
-  result = run_autotest(learner)
-  expect_true(result, info = result$error)
-})
