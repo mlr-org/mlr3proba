09ecb8a197b3f6c0ea9f4c1ecb545e96eda93666
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-04-16 14:01:46 +0100

    Update test_pecs.R

--- tests/testthat/test_pecs.R
+++ tests/testthat/test_pecs.R
@@ -4,6 +4,7 @@ set.seed(1)
 task = tgen("simsurv")$generate(5)
 
 test_that("pecs.list", {
+  skip_on_cran()
   learns = lrns(c("surv.kaplan", "surv.coxph"))
   expect_error(pecs(learns), "trained survival learners")
   suppressWarnings({lapply(learns, function(x) x$train(task))})
@@ -14,6 +15,7 @@ test_that("pecs.list", {
 })
 
 test_that("pecs.PredictionSurv", {
+  skip_on_cran()
   suppressWarnings({p = lrn("surv.coxph")$train(task)$predict(task)})
   expect_silent(pecs(p))
 })
