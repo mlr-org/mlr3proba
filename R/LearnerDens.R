cf695c1259b1f1ca1c15a4503c01ec1f82f8e8cb
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-04-25 14:06:57 +0100

    Update LearnerDens.R

--- R/LearnerDens.R
+++ R/LearnerDens.R
@@ -39,7 +39,7 @@ LearnerDens = R6::R6Class("LearnerDens", inherit = Learner,
                           man = NA_character_){
       super$initialize(id = id, task_type = "dens", param_set = param_set,
                        predict_types = predict_types, feature_types = feature_types, properties = properties,
-                       data_formats = data_formats, packages = packages,man = NA_character_)
+                       data_formats = data_formats, packages = packages, man = man)
     }
   )
 )
