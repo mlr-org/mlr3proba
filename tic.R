98a501c80505527a1cc7365400c551f75accc3ca
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2020-10-22 10:44:43 +0100

    Update tic.R

--- tic.R
+++ tic.R
@@ -2,11 +2,20 @@
 do_package_checks()
 
 if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
+  
+  get_stage("before_deploy") %>%
+    add_step(step_setup_push_deploy(branch = "master", orphan = FALSE))
+  
   # creates pkgdown site and pushes to gh-pages branch
   # only for the runner with the "BUILD_PKGDOWN" env var set
   get_stage("install") %>%
     add_step(step_install_github("mlr-org/mlr3pkgdowntemplate"))
+  
   do_pkgdown()
+  
+  get_stage("deploy") %>%
+    add_code_step(rmarkdown::render("README.Rmd")) %>%
+    add_step(step_do_push_deploy(commit_paths = c("README.md")))
 }
 
 get_stage("after_success") %>%
