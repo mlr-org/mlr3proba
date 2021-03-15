675450002d828302c01dde05b54df80a79adabab
Raphael Sonabend <raphaelsonabend@gmail.com>
2021-03-15 10:47:28 +0000

    suppress test warning

--- tests/testthat/test_resample.R
+++ tests/testthat/test_resample.R
@@ -5,6 +5,6 @@ test_that("resample-step", {
   learner = mlr_learners$get("surv.coxph")
   resampling = rsmp("cv", folds = 3)
 
-  rr = resample(task, learner, resampling)
+  rr = suppressWarnings(resample(task, learner, resampling))
   expect_resample_result(rr)
 })
