b5ceeb72183d8bb33e18e019fefd2bf172ad510b
Michel Lang <michellang@gmail.com>
2022-02-01 11:28:06 +0100

    add as_task_surv.formula method

--- R/as_task_surv.R
+++ R/as_task_surv.R
@@ -32,6 +32,11 @@ as_task_surv.TaskSurv = function(x, clone = FALSE, ...) { # nolint
 #'   Defaults to the (deparsed and substituted) name of `x`.
 #' @export
 as_task_surv.data.frame = function(x, time = "time", event = "event", time2, type = "right", id = deparse(substitute(x)), ...) { # nolint
+  ii = which(map_lgl(keep(x, is.double), anyInfinite))
+  if (length(ii)) {
+    warningf("Detected columns with unsupported Inf values in data: %s", str_collapse(names(ii)))
+  }
+
   TaskSurv$new(id = id, backend = x, time = time, event = event, time2 = time2, type = type)
 }
 
@@ -40,4 +45,24 @@ as_task_surv.data.frame = function(x, time = "time", event = "event", time2, typ
 #' @export
 as_task_surv.DataBackend = function(x, time = "time", event = "event", time2, type = "right", id = deparse(substitute(x)), ...) { # nolint
   TaskSurv$new(id = id, backend = x, time = time, event = event, time2 = time2, type = type)
+}
+
+#' @rdname as_task_surv
+#' @export
+as_task_surv.formula = function(x, data, id = deparse(substitute(data)), ...) { # nolint
+  all_vars = all.vars(x)
+  assert_subset(all_vars, c(names(data), "."), .var.name = "formula")
+  if (!attributes(terms(x, data = data))$response) {
+    stopf("Formula %s is missing a response", format(x))
+  }
+
+  tab = model.frame(x, data)
+  surv = stats::model.response(tab)
+  tab = cbind(
+    data.table(.__time__ = surv[, 1L], .__status__ = surv[, 2L]),
+    tab[, -1L, drop = FALSE]
+  )
+  setnames(tab, c(".__time__", ".__status__"), all_vars[1:2])
+
+  as_task_surv(tab, time = all_vars[1L], event = all_vars[2L], id = id)
 }
