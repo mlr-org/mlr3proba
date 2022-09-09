11bf5a8770d7db078224fc2fb1bb7e355e5cb7ca
john <bblodfon@gmail.com>
2022-09-09 12:54:35 +0300

    doc fix
    User should supply `-lp` in place of `lp`, when the linear predictor prediction
    comes from an AFT model.

--- R/surv_return.R
+++ R/surv_return.R
@@ -5,9 +5,12 @@
 #' @param surv (`matrix()`)\cr Matrix of predicted survival probabilities, rows are observations,
 #' columns are times. Number of columns should be equal to length of `times`.
 #' @param crank (`numeric()`)\cr Relative risk/continuous ranking. Higher value is associated
-#' with higher risk. If `NULL` then either set as as `-response` if available or
-#' `lp` if available or as the sum of the cumulative hazard function (expected
-#' mortality) derived from the predicted survival function (`surv`).
+#' with higher risk. If `NULL` then either set as `-response` if available or
+#' `lp` if available (this assumes that the `lp` prediction comes from a PH type
+#' model - in case of an AFT model the user should provide `-lp`).
+#' In case neither `response` or `lp` are provided, then `crank` is calculated
+#' as the sum of the cumulative hazard function (expected mortality) derived from
+#' the predicted survival function (`surv`).
 #' @param lp (`numeric()`)\cr Predicted linear predictor, used to impute `crank` if `NULL`.
 #' @param response (`numeric()`)\cr Predicted survival time, passed through function without
 #' modification.
