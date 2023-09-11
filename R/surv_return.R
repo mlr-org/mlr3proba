ff8cdbdf5dba3bb44b75e1f9479fda7938fd3809
john <bblodfon@gmail.com>
2023-09-11 11:49:58 +0200

    pass names when input is a vector and 'times' is not given

--- R/surv_return.R
+++ R/surv_return.R
@@ -6,7 +6,8 @@
 #' @param surv (`matrix()|array()`)\cr Matrix or array of predicted survival
 #' probabilities, rows (1st dimension) are observations, columns (2nd dimension)
 #' are times and in the case of an array there should be one more dimension.
-#' Number of columns should be equal to length of `times`.
+#' Number of columns should be equal to length of `times`. In case a `numeric()`
+#' vector is provided, it is converted to a single row (one observation) matrix.
 #' @param which.curve Which curve (3rd dimension) should the `crank` be
 #' calculated for, in case `surv` is an `array`? If between (0,1) it is taken as
 #' the quantile of the curves (assumes these are ordered) otherwise if greater
@@ -39,7 +40,8 @@
 
   if (!is.null(surv)) {
     if (class(surv)[1] == "numeric") {
-      surv = matrix(surv, nrow = 1)
+      # in case of a vector (one observation) convert to matrix
+      surv = matrix(surv, nrow = 1, dimnames = list(NULL, names(surv)))
     }
     if (class(surv)[1] == "array") {
       if (length(dim(surv)) != 3) {
@@ -47,7 +49,9 @@
       }
     }
     times = times %||% colnames(surv)
-    assert(length(times) == ncol(surv))
+    if (length(times) != ncol(surv)) {
+      stop("'times' must have the same length as the 2nd dimension (columns of 'surv')")
+    }
     colnames(surv) = times
   }
 
