15bbbb0e3d2f5882cb754b2911d3069e8e6262c8
john <bblodfon@gmail.com>
2023-12-20 16:53:19 +0100

    add tests for distrcompositor with breslow estimator

--- tests/testthat/test_pipeop_distrcompositor.R
+++ tests/testthat/test_pipeop_distrcompositor.R
@@ -2,7 +2,7 @@ test_that("PipeOpDistrCompositor - basic properties", {
   expect_pipeop(PipeOpDistrCompositor$new())
 })
 
-task = tsk("rats")$filter(sample(300, 10))
+task = tsk("rats")$filter(sample(300, 30))
 
 test_that("PipeOpDistrCompositor - overwrite = FALSE", {
   gr = mlr3pipelines::ppl("distrcompositor", lrn("surv.kaplan", id = "k2"), overwrite = FALSE)
@@ -10,6 +10,15 @@ test_that("PipeOpDistrCompositor - overwrite = FALSE", {
   expect_equal(
     gr$predict(task)[[1]]$data$distr,
     lrn("surv.kaplan", id = "k2")$train(task)$predict(task)$data$distr)
+
+  # breslow
+  gr = mlr3pipelines::ppl("distrcompositor", lrn("surv.coxph"),
+    estimator = "breslow", overwrite = FALSE)
+  expect_silent(gr$train(task))
+  expect_equal(
+    gr$predict(task)[[1]]$data$distr,
+    lrn("surv.coxph")$train(task)$predict(task)$data$distr
+  )
 })
 
 test_that("PipeOpDistrCompositor - overwrite = TRUE", {
@@ -22,8 +31,15 @@ test_that("PipeOpDistrCompositor - overwrite = TRUE", {
   gr = mlr3pipelines::ppl("distrcompositor", lrn("surv.kaplan", id = "k2"), overwrite = TRUE, form = "po",
     graph_learner = TRUE)
   expect_prediction_surv(gr$train(task)$predict(task))
-})
 
+  # breslow
+  gr = mlr3pipelines::ppl("distrcompositor", lrn("surv.coxph"), estimator = "breslow",
+    overwrite = TRUE, graph_learner = TRUE)
+  expect_silent(gr$train(task))
+  surv_mat1 = gr$predict(task)$data$distr
+  surv_mat2 = lrn("surv.coxph")$train(task)$predict(task)$data$distr
+  expect_false(all(surv_mat1 == surv_mat2)) # distr predictions changed (a bit)
+})
 
 test_that("no params", {
   base = lrn("surv.kaplan")$train(task)$predict(task)
