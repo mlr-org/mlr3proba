762f100e96e9689e76b354f7e2e268f9a9d215c9
john <bblodfon@gmail.com>
2024-02-16 13:51:01 +0100

    refine doc for calib_alpha

--- R/MeasureSurvCalibrationAlpha.R
+++ R/MeasureSurvCalibrationAlpha.R
@@ -1,17 +1,28 @@
 #' @template surv_measure
-#' @templateVar title Van Houwelingen's Alpha
+#' @templateVar title Van Houwelingen's Calibration Alpha
 #' @templateVar fullname MeasureSurvCalibrationAlpha
 #'
 #' @description
 #' This calibration method is defined by estimating
-#' \deqn{\alpha = \sum \delta_i / \sum H_i(t_i)}
-#' where \eqn{\delta} is the observed censoring indicator from the test data, \eqn{H_i} is the
-#' predicted cumulative hazard, and \eqn{t_i} is the observed survival time.
+#' \deqn{\hat{\alpha} = \sum \delta_i / \sum H_i(T_i)}
+#' where \eqn{\delta} is the observed censoring indicator from the test data,
+#' \eqn{H_i} is the predicted cumulative hazard, and \eqn{T_i} is the observed
+#' survival time (event or censoring).
 #'
 #' The standard error is given by
-#' \deqn{exp(1/\sqrt{\sum \delta_i})}
+#' \deqn{\hat{\alpha_{se}} = exp(1/\sqrt{\sum \delta_i})}
 #'
-#' The model is well calibrated if the estimated \eqn{\alpha} coefficient is equal to 1.
+#' The model is well calibrated if the estimated \eqn{\hat{\alpha}} coefficient
+#' (returned score) is equal to 1.
+#'
+#' @section Parameter details:
+#' - `se` (`logical(1)`)\cr
+#' If `TRUE` then return standard error of the measure, otherwise the score
+#' itself (default).
+#' - `method` (`character(1)`)\cr
+#' Returns \eqn{\hat{\alpha}} if equal to `ratio` (default) and \eqn{|1-\hat{\alpha}|} if equal to `diff`.
+#' With `diff`, the output score can be minimized and for example be used for tuning purposes.
+#' This parameter takes effect only if `se` is `FALSE`.
 #'
 #' @references
 #' `r format_bib("vanhouwelingen_2000")`
@@ -23,17 +34,23 @@ MeasureSurvCalibrationAlpha = R6Class("MeasureSurvCalibrationAlpha",
   inherit = MeasureSurv,
   public = list(
     #' @description Creates a new instance of this [R6][R6::R6Class] class.
-    initialize = function() {
+    #' @param method defines which output score to return, see "Parameter
+    #' details" section.
+    initialize = function(method = "ratio") {
+      assert_choice(method, choices = c("ratio", "diff"))
+
       ps = ps(
         se = p_lgl(default = FALSE),
         method = p_fct(c("ratio", "diff"), default = "ratio")
       )
-      ps$values = list(se = FALSE, method = "ratio")
+      ps$values = list(se = FALSE, method = method)
+      range = if (method == "ratio") c(-Inf, Inf) else c(0, Inf)
+      minimize = ifelse(method == "ratio", FALSE, TRUE)
 
       super$initialize(
         id = "surv.calib_alpha",
-        range = c(-Inf, Inf),
-        minimize = FALSE,
+        range = range,
+        minimize = minimize,
         predict_type = "distr",
         label = "Van Houwelingen's Alpha",
         man = "mlr3proba::mlr_measures_surv.calib_alpha",
