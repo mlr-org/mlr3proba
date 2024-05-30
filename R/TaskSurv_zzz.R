528019d2412e10caa2e7bcad692e6e1ecdc6ab0d
john <bblodfon@gmail.com>
2024-05-31 00:11:19 +0200

    add gbsg, mgus and pbc datasets from survival package

--- R/TaskSurv_zzz.R
+++ R/TaskSurv_zzz.R
@@ -1,3 +1,110 @@
+#' @title German Breast Cancer Study Survival Task
+#'
+#' @name mlr_tasks_gbsg
+#' @templateVar type Surv
+#' @templateVar task_type survival
+#' @templateVar id gbsg
+#' @templateVar data gbsg
+#' @template task
+#' @template seealso_task
+#'
+#' @section Pre-processing:
+#' - Removed column `pid`.
+#' - Column `meno` has been converted to `factor` and 0/1 values have been
+#' replaced with `premenopausal` and `postmenopausal` respectively.
+#' - Column `hormon` has been converted to `factor` and 0/1 values have been
+#' replaced with `no` and `yes` respectively.
+#' - Column `grade` has been converted to `factor`.
+#' - Renamed target column `rfstime` to `time`.
+NULL
+
+load_gbsg = function() {
+  data = survival::gbsg
+  data$pid = NULL
+  data$meno = factor(ifelse(data$meno == 0, "premenopausal", "postmenopausal"),
+                     levels = c("premenopausal", "postmenopausal"))
+  data$hormon = factor(ifelse(data$hormon == 0, "no", "yes"),
+                       levels = c("no", "yes"))
+  data$grade = factor(data$grade)
+  colnames(data)[colnames(data) == "rfstime"] = "time"
+
+  b = as_data_backend(data)
+  task = TaskSurv$new("gbsg", b, time = "time", event = "status",
+                      label = "German Breast Cancer")
+  b$hash = task$man = "mlr3proba::mlr_tasks_gbsc"
+
+  task
+}
+
+#' @title Primary Biliary Cholangitis Survival Task
+#'
+#' @name mlr_tasks_pbc
+#' @templateVar type Surv
+#' @templateVar task_type survival
+#' @templateVar id pbc
+#' @templateVar data pbc
+#' @template task
+#' @template seealso_task
+#'
+#' @section Pre-processing:
+#' - Removed column `id`.
+#' - Kept only complete cases (no missing values).
+#' - Column `age` has been converted to `integer`.
+#' - Columns `trt`, `stage`, `hepato`, `edema` and `ascites` have been converted
+#' to `factor`s.
+#' - Column `trt` has levels `Dpenicillmain` and `placebo` instead of 1 and 2.
+#' - Column `status` has 1 for death and 0 for censored or transplant.
+NULL
+
+load_pbc = function() {
+  data = survival::pbc
+  data = na.omit(data)
+  data$id = NULL
+  data = map_at(data, c("age"), as.integer)
+  data = map_at(data, c("spiders", "hepato", "edema", "ascites"), as.factor)
+  data$trt = factor(ifelse(data$trt == 1, "Dpenicillmain", "placebo"),
+                    levels = c("Dpenicillmain", "placebo"))
+  data$stage = factor(data$stage)
+  data$status[data$status > 0] = data$status[data$status > 0] - 1
+
+  b = as_data_backend(data)
+  task = TaskSurv$new("pbc", b, time = "time", event = "status",
+                      label = "Primary Biliary Cholangitis")
+  b$hash = task$man = "mlr3proba::mlr_tasks_pbc"
+
+  task
+}
+
+#' @title Monoclonal Gammopathy Survival Task
+#'
+#' @name mlr_tasks_mgus
+#' @templateVar type Surv
+#' @templateVar task_type survival
+#' @templateVar id mgus
+#' @templateVar data mgus
+#' @template task
+#' @template seealso_task
+#'
+#' @section Pre-processing:
+#' - Removed columns `id`, `pcdx` and `pctime`.
+#' - Renamed target columns from (`fultime`, `death`) to (`time`, `status`).
+#' - Kept only complete cases (no missing values).
+NULL
+
+load_mgus = function() {
+  data = survival::mgus
+  data[, c("id", "pcdx", "pctime")] = NULL
+  colnames(data)[colnames(data) == "futime"] = "time"
+  colnames(data)[colnames(data) == "death"] = "status"
+  data = na.omit(data)
+
+  b = as_data_backend(data)
+  task = TaskSurv$new("mgus", b, time = "time", event = "status", label = "MGUS")
+  b$hash = task$man = "mlr3proba::mlr_tasks_mgus"
+
+  task
+}
+
 #' @title Veteran Survival Task
 #'
 #' @name mlr_tasks_veteran
@@ -229,12 +336,16 @@ load_whas = function() {
   colnames(data)[10:11] = c("time", "status")
 
   b = as_data_backend(data)
-  task = TaskSurv$new("whas", b, time = "time", event = "status", label = "Worcester Heart Attack")
+  task = TaskSurv$new("whas", b, time = "time", event = "status",
+                      label = "Worcester Heart Attack")
   b$hash = task$man = "mlr3proba::mlr_tasks_whas"
 
   task
 }
 
+register_task("gbsg", load_gbsg)
+register_task("pbc", load_pbc)
+register_task("mgus", load_mgus)
 register_task("veteran", load_veteran)
 register_task("rats", load_rats)
 register_task("unemployment", load_unemployment)
