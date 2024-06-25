b8b25266e05758fbc894fc0ab119e6c7afaf911c
Philip Studener <philip.studener@gmx.de>
2024-06-25 10:52:32 +0200

    add pammtools to suggests

--- tests/testthat/test_pipeline_survtoclassif.R
+++ tests/testthat/test_pipeline_survtoclassif.R
@@ -35,23 +35,4 @@ test_that("survtoclassif", {
   pipe$train(task) |> suppressWarnings()
   p = pipe$predict(task)
   expect_prediction_surv(p[[1]])
-
-
-  dt = task$data()
-  dt[, status := factor(status)]
-  long_data = discSurv::dataLong(as.data.frame(dt),
-                                 timeColumn = "time", eventColumn = "status")
-
-  long_data$tend = long_data$timeInt
-
-  discSurv_model = glm(y ~ litter + rx + sex + tend, data = long_data,
-                       family = "binomial", model = FALSE) |> suppressWarnings()
-
-  po = mlr3pipelines::po("trafotask_survclassif",
-                         cut = seq_len(task$data()$time |> max() |> as.integer()))
-  trafo = po$train(list(task))
-  learner = lrn("classif.log_reg", predict_type = "prob")
-  learner$train(trafo[[1]]) |> suppressWarnings()
-
-  expect_equal(learner$model$coefficients, discSurv_model$coefficients)
 })
