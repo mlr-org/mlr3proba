7f0c5f3bbce00622b494ad793b4c0c7ab820e421
john <bblodfon@gmail.com>
2024-06-27 20:02:29 +0200

    move pipeline code to appropriate file

--- R/pipelines.R
+++ R/pipelines.R
@@ -519,9 +519,53 @@ pipeline_survtoregr = function(method = 1, regr_learner = lrn("regr.featureless"
   gr
 }
 
+#' @name mlr_graphs_survtoclassif
+#' @title Survival to Classification Reduction Pipeline
+#' @description Wrapper around multiple [PipeOp][mlr3pipelines::PipeOp]s to help in creation
+#' of complex survival to reduction methods.
+#'
+#' @param learner [LearnerClassif][mlr3::LearnerClassif]\cr
+#' Classification learner to fit to the transformed [TaskClassif][mlr3::TaskClassif]. `learner` must have `prob` predict_type.
+#' @param cut `numeric()`\cr
+#' Split points, used to partition the data into intervals.
+#' If unspecified, all unique event times will be used.
+#' @param max_time `numeric(1)`\cr
+#' If cut is unspecified, this will be the last possible event time.
+#' All event times after max_time will be administratively censored at max_time.
+#'
+#' @return [mlr3pipelines::Graph] or [mlr3pipelines::GraphLearner]
+#' @family pipelines
+#'
+#' @examples
+#' \dontrun{
+#' if (requireNamespace("mlr3pipelines", quietly = TRUE)) {
+#'   library("mlr3")
+#'   library("mlr3pipelines")
+#'
+#'   task = tsk("rats")
+#'
+#'   pipe = ppl(
+#'     "survtoclassif",
+#'     learner = lrn("classif.log_reg")
+#'   )
+#'   pipe$train(task)
+#'   pipe$predict(task)
+#'
+#' }
+#' }
+#' @export
+pipeline_survtoclassif = function(learner, cut = NULL, max_time = NULL) {
+  gr = mlr3pipelines::po("trafotask_survclassif", cut = cut, max_time = max_time) |>
+    mlr3pipelines::`%>>%`(list(mlr3pipelines::po("learner", learner, predict_type = "prob"), mlr3pipelines::po("nop"))) |>
+    mlr3pipelines::`%>>%`(mlr3pipelines::po("trafopred_classifsurv"))
+
+  gr
+}
+
 register_graph("survaverager", pipeline_survaverager)
 register_graph("survbagging", pipeline_survbagging)
 register_graph("crankcompositor", pipeline_crankcompositor)
 register_graph("distrcompositor", pipeline_distrcompositor)
 register_graph("probregr", pipeline_probregr)
 register_graph("survtoregr", pipeline_survtoregr)
+register_graph("survtoclassif", pipeline_survtoclassif)
