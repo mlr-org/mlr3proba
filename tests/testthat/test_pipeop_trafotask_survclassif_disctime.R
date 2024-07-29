e2b471c63b920e8c35226f7822032ed44d99759b
john <bblodfon@gmail.com>
2024-07-29 23:14:07 +0200

    update test, split train/test

--- tests/testthat/test_pipeop_trafotask_survclassif_disctime.R
+++ tests/testthat/test_pipeop_trafotask_survclassif_disctime.R
@@ -1,13 +1,18 @@
 test_that("PipeOpTaskSurvClassifDiscTime", {
   task = tsk("lung")
+
+  # imitate train/test split manually
   test_ids = c(2, 10, 107)
+  train_ids = setdiff(task$row_ids, test_ids)
   test_task = task$clone()$filter(rows = test_ids)
-  expect_equal(test_ids, test_task$row_ids)
+  train_task = task$clone()$filter(rows = train_ids)
+  expect_equal(test_task$row_ids, test_ids)
+  expect_equal(train_task$row_ids, train_ids)
 
   po_disc = mlr3pipelines::po("trafotask_survclassif_disctime", cut = 4)
   expect_class(po_disc, c("PipeOp", "PipeOpTaskSurvClassifDiscTime"))
 
-  res = po_disc$train(list(task))
+  res = po_disc$train(list(train_task))
 
   # 0 is added
   time_cuts = po_disc$state$cut
@@ -17,6 +22,7 @@ test_that("PipeOpTaskSurvClassifDiscTime", {
   # classification task
   output_task = res[[1L]]
   expect_task_classif(output_task)
+  expect_equal(output_task$col_roles$original_ids, "id")
   expect_equal(output_task$positive, "1")
   expect_equal(output_task$target_names, "ped_status")
   # new column added to the task
@@ -34,23 +40,26 @@ test_that("PipeOpTaskSurvClassifDiscTime", {
   tends = pred_task$data(cols = "tend")[[1L]]
   expect_equal(sort(unique(tends)), time_cuts[2:5])
   # test row ids are correct
-  expect_equal(pred_task$row_names$row_name, rep(test_ids, each = 4))
+  expect_equal(pred_task$col_roles$original_ids, "id")
+  original_ids = pred_task$data(cols = "id")[[1L]]
+  correct_ids = rep(test_ids, each = 4)
+  expect_equal(original_ids, correct_ids)
 
   transformed_data = res[["transformed_data"]]
   # test rows ids are correct
-  expect_equal(transformed_data$id, rep(test_ids, each = 4))
+  expect_equal(transformed_data$id, correct_ids)
   # check columns in the transformed data.table
-  expect_equal(sort(c("id", "ped_status", "time2", pred_task$feature_names)),
+  expect_equal(sort(c("id", "ped_status", "obs_times", pred_task$feature_names)),
                sort(colnames(transformed_data)))
 
   # `ped_status` per interval and per observation is correct
-  # before observed time ("time2"), "ped_status" = 0
-  expect_equal(as.character(unique(transformed_data[tend < time2, ped_status])),
+  # before observed time ("obs_times"), "ped_status" = 0
+  expect_equal(as.character(unique(transformed_data[tend < obs_times, ped_status])),
                "0")
   times = test_task$times() # observed times
   status = as.character(test_task$status())
   # after observed time, "ped_status" must be the same as "status"
-  td = transformed_data[tend > time2]
+  td = transformed_data[tend > obs_times]
   expect_equal(as.character(unique(td[id == test_ids[1], ped_status])), status[1])
   expect_equal(as.character(unique(td[id == test_ids[2], ped_status])), status[2])
   expect_equal(as.character(unique(td[id == test_ids[3], ped_status])), status[3])
