c476aed01d38f5bac60a138759538c17319b1b9d
john <bblodfon@gmail.com>
2024-09-17 16:48:55 +0200

    refine IPCW test

--- tests/testthat/test_ipcw.R
+++ tests/testthat/test_ipcw.R
@@ -15,8 +15,9 @@ test_that("PipeOpTaskSurvClassifIPCW", {
   expect_error(po_ipcw$train(list(train_task)), "not 'NULL'")
   # `cutoff_time` should be less than the max event time
   po_ipcw$param_set$set_values(cutoff_time = 3000)
-  expect_error(po_ipcw$train(list(train_task)), "cutoff_time < max_event_time is not TRUE")
-  po_ipcw$param_set$set_values(cutoff_time = 300)
+  expect_error(po_ipcw$train(list(train_task)), "cutoff_time <= max_event_time is not TRUE")
+  cutoff = 300
+  po_ipcw$param_set$set_values(cutoff_time = cutoff)
 
   res = po_ipcw$train(list(train_task))
 
@@ -29,22 +30,26 @@ test_that("PipeOpTaskSurvClassifIPCW", {
   expect_equal(sum(output_task$weights$weight), output_task$nrow)
   expect_equal(output_task$positive, "1")
   expect_equal(output_task$target_names, "status")
-  expect_equal(output_task$nrow, train_task$nrow) # same observations
+  expect_equal(output_task$nrow, train_task$nrow) # same #observations
 
-  # check: do `output_task$weights` make sense? are 0 the ones that should be 0?
-  zero_weight_rows = train_task$status() == 0 & train_task$times() < 300
-  expect_true(all(output_task$weights$weight[zero_weight_rows] == 0))
+  # check: are weights = 0 the ones that should be so?
+  true_zero_weight_ids = which(train_task$status() == 0 & train_task$times() <= cutoff)
+  res_zero_weight_ids = which(output_task$weights$weight == 0)
+  expect_equal(res_zero_weight_ids, true_zero_weight_ids)
 
   res = po_ipcw$predict(list(test_task))
   pred_task = res[[1L]]
 
   expect_task_classif(pred_task)
   # check status == 0 for time > cutoff time
-  expect_true(all(pred_task$data(,"status")[res$data$times > 300] == 0))
+  expect_true(all(pred_task$truth()[res$data$times > cutoff] == 0))
 
   # (row_ids, times, status, cutoff_time) are correct?
   data = res[[2L]]
   expect_list(data, len = 4)
-  expect_true(length(data$row_ids) == length(test_ids))
   expect_equal(names(data), c("row_ids", "times", "status", "cutoff_time"))
+  expect_equal(data$row_ids, test_ids)
+  expect_equal(data$times, test_task$times())
+  expect_equal(data$status, test_task$status())
+  expect_equal(data$cutoff_time, cutoff)
 })
