c8d4b9fc09239cbfe568dec8b44cc0b4b4d1c34a
john <bblodfon@gmail.com>
2024-10-08 16:17:55 +0200

    add doc template for pipeline construction via ppl

--- man-roxygen/pipeline.R
+++ man-roxygen/pipeline.R
@@ -4,6 +4,14 @@
 #' @param graph_learner (`logical(1)`)\cr
 #' If `TRUE` returns wraps the [Graph][mlr3pipelines::Graph] as a
 #' [GraphLearner][mlr3pipelines::GraphLearner] otherwise (default) returns as a `Graph`.
+#' @section Dictionary:
+#' This [Graph][mlr3pipelines::Graph] can be instantiated via the [dictionary][mlr3misc::Dictionary]
+#' [mlr_graphs][mlr3pipelines::mlr_graphs] or with the associated sugar function [ppl()][mlr3pipelines::ppl]:
+#' ```
+#' mlr_graphs$get("<%=id%>")
+#' ppl("<%=id%>")
+#' ```
+#'
 #' @return [mlr3pipelines::Graph] or [mlr3pipelines::GraphLearner]
 #' @family pipelines
 #' @export
