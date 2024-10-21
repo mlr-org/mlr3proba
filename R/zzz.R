5133cf3a708fe7a5ed23d3de86001cc03e8ffae6
john <bblodfon@gmail.com>
2024-10-21 22:55:28 +0200

    import mlr3pipelines functions and shorthand constructors

--- R/zzz.R
+++ R/zzz.R
@@ -28,10 +28,11 @@ NULL
 #' @import paradox
 #' @importFrom R6 R6Class
 #' @importFrom utils data head tail
-#' @importFrom stats reformulate model.matrix model.frame sd predict complete.cases density
+#' @importFrom stats model.matrix model.frame sd predict density
 #' @importFrom survival Surv
 #' @importFrom mlr3viz fortify
 #' @importFrom utils getFromNamespace
+#' @importFrom mlr3pipelines po as_graph %>>% pipeline_greplicate gunion Graph ppl
 "_PACKAGE"
 # nolint end
 
