86d46b83af6999d430593a1bcab4d0236f3f3235
john <bblodfon@gmail.com>
2025-01-04 00:19:29 +0200

    refactor: autoplot.PredictionSurv

--- R/autoplot.R
+++ R/autoplot.R
@@ -42,7 +42,7 @@
 #' autoplot(task, type = "duo")
 #' @export
 autoplot.TaskSurv = function(object, type = "target", theme = theme_minimal(), reverse = FALSE, ...) { # nolint
-  assert_string(type)
+  assert_choice(type, choices = c("target", "duo", "pairs"), null.ok = FALSE)
   require_namespaces(c("survival", "GGally"))
 
   switch(type,
@@ -91,7 +91,7 @@ plot.TaskSurv = function(x, ...) {
 #'   * `"freq"`: histogram frequency plot with [ggplot2::geom_histogram()].
 #'   * `"overlay"`: histogram with overlaid density plot with [ggplot2::geom_histogram()] and
 #'   [ggplot2::geom_density()].
-#'   * `"freqpoly"`: frequency polygon plot with `ggplot2::geom_freqpoly`.
+#'   * `"freqpoly"`: frequency polygon plot with [ggplot2::geom_freqpoly].
 #' @template param_theme
 #' @param ... (`any`):
 #'   Additional arguments, possibly passed down to the underlying plot functions.
@@ -113,30 +113,40 @@ plot.TaskSurv = function(x, ...) {
 #' autoplot(task, type = "freqpoly", bins = 15)
 #' @export
 autoplot.TaskDens = function(object, type = "dens", theme = theme_minimal(), ...) { # nolint
-  assert_choice(type, c("dens", "freq", "overlay", "freqpoly"))
+  assert_choice(type, c("dens", "freq", "overlay", "freqpoly"), null.ok = FALSE)
 
   p = ggplot(data = object, aes(x = .data[[object$feature_names]]), ...)
 
-  if (type == "dens") {
-    p +
-      geom_histogram(aes(y = after_stat(density)), fill = "white", color = "black", ...) +
-      ylab("Density") +
-      theme
-  } else if (type == "freq") {
-    p + geom_histogram(fill = "white", color = "black", ...) +
-      ylab("Count") +
-      theme
-  } else if (type == "overlay") {
-    p +
-      geom_histogram(aes(y = after_stat(density)), colour = "black", fill = "white", ...) +
-      geom_density(alpha = 0.2, fill = "#5dadc8") +
-      ylab("Density") +
-      theme
-  } else {
-    p +
-      geom_freqpoly(...) +
-      theme
-  }
+  switch(type,
+    "dens" = {
+      p +
+        geom_histogram(aes(y = after_stat(density)), fill = "white", color = "black", ...) +
+        ylab("Density") +
+        theme
+    },
+
+    "freq" = {
+      p + geom_histogram(fill = "white", color = "black", ...) +
+        ylab("Count") +
+        theme
+    },
+
+    "overlay" = {
+      p +
+        geom_histogram(aes(y = after_stat(density)), colour = "black", fill = "white", ...) +
+        geom_density(alpha = 0.2, fill = "#5dadc8") +
+        ylab("Density") +
+        theme
+    },
+
+    "freqpoly" = {
+      p +
+        geom_freqpoly(...) +
+        theme
+    },
+
+    stopf("Unknown plot type '%s'", type)
+  )
 }
 
 #' @export
@@ -149,32 +159,41 @@ plot.TaskDens = function(x, ...) {
 #' @description
 #' Generates plots for [mlr3proba::PredictionSurv], depending on argument `type`:
 #'
-#' * `"calib"` (default): Calibration plot comparing the average predicted survival distribution
-#'   to a Kaplan-Meier prediction, this is *not* a comparison of a stratified `crank` or `lp`
-#'   prediction. `object` must have `distr` prediction. `geom_line()` is used for comparison split
-#'   between the prediction (`Pred`) and Kaplan-Meier estimate (`KM`). In addition labels are added
-#'   for the x (`T`) and y (`S(T)`) axes.
-#' * `"dcalib"`: Distribution calibration plot. A model is D-calibrated if X% of deaths occur before
-#'   the X/100 quantile of the predicted distribution, e.g. if 50% of observations die before their
-#'   predicted median survival time. A model is D-calibrated if the resulting plot lies on x = y.
-#' * `"preds"`: Matplots the survival curves for all predictions
+#' - `"calib"` (default): **Calibration plot** comparing the average predicted
+#' survival distribution (`Pred`) to a Kaplan-Meier prediction (`KM`), this is
+#' *not* a comparison of a stratified `crank` or `lp`.
+#' - `"dcalib"`: **Distribution calibration plot**.
+#' A model is considered D-calibrated if, for any given quantile `p`, the
+#' proportion of observed outcomes occurring before the predicted time quantile,
+#' matches `p`. For example, 50% of events should occur before the predicted
+#' median survival time (i.e. the time corresponding to a predicted survival
+#' probability of 0.5).
+#' This means that the resulting line plot will lie close to the straight line
+#' y = x.
+#' Note that we impute `NA`s from the predicted quantile function with the
+#' maximum observed outcome time.
+#' - `"isd"`: Plot the predicted **i**ndividual **s**urvival **d**istributions
+#' (survival curves) for observations from the test set.
+#'
+#' @section Notes:
+#'
+#' 1. `object` must have a `distr` prediction, as all plot `type`s use the
+#' predicted survival distribution/matrix.
+#' 2. `type = "dcalib"` is drawn a bit differently from Haider et al. (2020),
+#' though its still conceptually the same.
 #'
 #' @param object ([mlr3proba::PredictionSurv]).
-#' @template param_type
-#' @param task ([mlr3proba::TaskSurv]) \cr
-#'   If `type = "calib"` then `task` is passed to `$predict` in the Kaplan-Meier learner.
+#' @param type (`character(1)`) \cr
+#'  Type of the plot, see Description.
 #' @param row_ids (`integer()`) \cr
-#'   If `type = "calib"` then `row_ids` is passed to `$predict` in the Kaplan-Meier learner.
+#'  If `type = "isd"`, specific observation ids (from the test set) for which
+#'  we draw their predicted survival distributions.
 #' @param times (`numeric()`) \cr
-#'   If `type = "calib"` then `times` is the values on the x-axis to plot over,
-#'    if `NULL` uses all times from `task`.
-#' @param xyline (`logical(1)`) \cr
-#'   If `TRUE` (default) plots the x-y line for `type = "dcalib"`.
+#'  If `type = "calib"` then `times` is the values on the x-axis to plot over.
+#'  if `NULL` uses all time points from the predicted survival matrix (`object$data$distr`).
 #' @param cuts (`integer(1)`) \cr
-#'   Number of cuts in (0,1) to plot `dcalib` over, default is `11`.
+#'  Number of cuts in \eqn{(0,1)} to plot `dcalib` over, default is `11`.
 #' @template param_theme
-#' @param extend_quantile `(logical(1))` \cr
-#'  If `TRUE` then `dcalib` will impute NAs from predicted quantile function with the maximum observed outcome time, e.g. if the last predicted survival probability is greater than 0.1, then the last predicted cdf is smaller than 0.9 so F^1(0.9) = NA, this would be imputed with max(times). Default is `FALSE`.
 #' @param ... (`any`):
 #'   Additional arguments, currently unused.
 #'
@@ -193,83 +212,122 @@ plot.TaskDens = function(x, ...) {
 #' p = learner$train(task, row_ids = 1:300)$predict(task, row_ids = 301:400)
 #'
 #' # calibration by comparison of average prediction to Kaplan-Meier
-#' autoplot(p, type = "calib", task = task, row_ids = 301:400)
+#' autoplot(p)
+#'
+#' # same as above, use specific time points
+#' autoplot(p, times = seq(1, 1000, 5))
 #'
 #' # Distribution-calibration (D-Calibration)
-#' autoplot(p, type = "dcalib", extend_quantile = TRUE)
+#' autoplot(p, type = "dcalib")
 #'
-#' # Predictions
-#' autoplot(p, type = "preds")
+#' # Predicted survival curves (all observations)
+#' autoplot(p, type = "isd")
+#'
+#' # Predicted survival curves (specific observations)
+#' autoplot(p, type = "isd", row_ids = c(301, 351, 399))
+#'
 #' @export
 autoplot.PredictionSurv = function(object, type = "calib",
-  task = NULL, row_ids = NULL, times = NULL, xyline = TRUE,
-  cuts = 11L, theme = theme_minimal(), extend_quantile = FALSE, ...) {
-
+  times = NULL, row_ids = NULL, cuts = 11L, theme = theme_minimal(), ...) {
+  assert_choice(type, c("calib", "dcalib", "isd"), null.ok = FALSE)
   assert("distr" %in% object$predict_types)
+  assert_number(cuts, na.ok = FALSE, lower = 1L, null.ok = FALSE)
+  assert_numeric(row_ids, any.missing = FALSE, lower = 1, null.ok = TRUE)
 
   switch(type,
     "calib" = {
-      assert_task(task)
-      if (is.null(times)) {
-        times = sort(unique(task$truth()[, 1L]))
-      }
-
-      if (inherits(object$distr, "VectorDistribution")) {
-        pred_surv = 1 - distr6::as.MixtureDistribution(object$distr)$cdf(times)
+      # get predicted survival matrix
+      if (inherits(object$data$distr, "array")) {
+        surv = object$data$distr
+        if (length(dim(surv)) == 3L) {
+          # survival 3d array, extract median
+          surv = .ext_surv_mat(arr = surv, which.curve = 0.5)
+        }
       } else {
-        pred_surv = rowMeans(1 - object$distr$cdf(times))
+        stop("Distribution prediction does not have a survival matrix or array
+             in the $data$distr slot")
       }
+      # get predicted time points
+      pred_times = as.numeric(colnames(surv))
+      # which time points to use for plotting
+      times = times %??% pred_times
 
-      km = lrn("surv.kaplan")
-      km_pred = km$train(task, row_ids = row_ids)$predict(task, row_ids = row_ids)
-      km_surv = rowMeans(1 - km_pred$distr$cdf(times))
+      # function to request S(t) for points "in-between"
+      extend_times = getFromNamespace("C_Vec_WeightedDiscreteCdf", ns = "distr6")
+      # rows => times, cols => obs
+      surv2 = extend_times(times, pred_times, cdf = t(1 - surv), FALSE, FALSE)
 
-      data = data.frame(x = times, y = c(km_surv, pred_surv),
-        Group = rep(c("KM", "Pred"), each = length(times)))
+      # average predicted probability across test set observations
+      pred_surv = rowMeans(surv2)
 
-      ggplot(data, aes(x = .data[["x"]], y = .data[["y"]], group = .data[["Group"]], color = .data[["Group"]])) +
+      # fit a Kaplan-Meier on the test data
+      km_fit = survival::survfit(object$truth ~ 1)
+      # make a S(t) one-column matrix (by default same probability for every observation)
+      km_surv = matrix(km_fit$surv, ncol = 1) # rows => times
+      # get KM's S(t) at the predicted time points
+      km_surv = extend_times(times, km_fit$time, cdf = 1 - km_surv, FALSE, FALSE)[,1]
+
+      data = data.table(
+        x = times,
+        y = c(km_surv, pred_surv),
+        Group = rep(c("KM", "Pred"), each = length(times))
+      )
+
+      ggplot(data, aes(x = .data[["x"]], y = .data[["y"]], group = .data[["Group"]],
+                       color = .data[["Group"]])) +
         geom_line() +
-        labs(x = "T", y = "S(T)") +
+        labs(x = "Time", y = "Average Survival Probability") +
         theme +
         theme(legend.title = element_blank())
-
     },
 
     "dcalib" = {
       p = seq.int(0, 1, length.out = cuts)
       true_times = object$truth[, 1L]
       q = map_dbl(p, function(.x) {
+        # time points at which observations had `.x` survival
         qi = as.numeric(object$distr$quantile(.x))
-        if (extend_quantile) {
-          qi[is.na(qi)] = max(true_times)
-        }
+        qi[is.na(qi)] = max(true_times)
         sum(true_times <= qi) / length(object$row_ids)
       })
-      pl = ggplot(data = data.frame(p, q), aes(x = p, y = q)) +
-        geom_line()
 
-      if (xyline) {
-        pl = pl +
-          annotate("segment", x = 0, y = 0, xend = 1, yend = 1, color = "lightgray")
-      }
-      pl +
-        labs(x = "True", y = "Predicted") +
+      ggplot(data = data.table(p, q), aes(x = p, y = q)) +
+        geom_bar(stat = "identity", fill = "skyblue", color = "black") +
+        geom_line(color = "red") +
+        scale_x_continuous(breaks = p) +
+        annotate("segment", x = 0, y = 0, xend = 1, yend = 1, color = "black",
+                 linetype = "dashed") +
+        labs(x = "Survival Probability (Bins)",
+             y = "Observed Proportion") +
         theme
     },
 
-    "preds" = {
-      v = 1 - distr6::gprm(object$distr, "cdf")
-      surv = data.frame(
-        Var1 = as.factor(seq_len(nrow(v))),
-        Var2 = rep(as.numeric(colnames(v)), each = nrow(v)),
-        value = invoke(c, .args = as.data.frame(v))
+    "isd" = {
+      surv = object$data$distr # assume this is 2d survival matrix
+      data = data.table(
+        row_id = as.factor(object$row_ids),
+        time = rep(as.numeric(colnames(surv)), each = nrow(surv)),
+        surv_prob = invoke(c, .args = as.data.table(surv))
       )
 
-      ggplot(surv, aes(x = .data[["Var2"]], y = .data[["value"]], group = .data[["Var1"]], color = .data[["Var1"]])) +
+      # filter data to specific ids
+      if (!is.null(row_ids)) {
+        data = data[row_id %in% row_ids]
+      }
+
+      p =
+        ggplot(data, aes(x = .data[["time"]], y = .data[["surv_prob"]],
+                         group = .data[["row_id"]], color = .data[["row_id"]])) +
         geom_line() +
-        labs(x = "T", y = "S(T)") +
-        theme +
-        theme(legend.position = "n")
+        labs(x = "Time", y = "Survival Probability") +
+        theme
+
+      # usually too many observations, so don't draw legend
+      if (is.null(row_ids)) {
+        p = p + theme(legend.position = "none")
+      }
+
+      p
     },
 
     stopf("Unknown plot type '%s'", type)
