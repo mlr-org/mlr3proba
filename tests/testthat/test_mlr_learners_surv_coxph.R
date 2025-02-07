0db176940d59d5ab88f85e430b8f173cafcbd93d
john <bblodfon@gmail.com>
2025-02-07 14:11:23 +0100

    refine error expectation

--- tests/testthat/test_mlr_learners_surv_coxph.R
+++ tests/testthat/test_mlr_learners_surv_coxph.R
@@ -12,5 +12,5 @@ test_that("missing", {
   lung = survival::lung
   lung$status = lung$status - 1
   task = mlr3proba::TaskSurv$new("lung", backend = lung, time = "time", event = "status")
-  expect_error(learner$predict(task))
+  expect_error(learner$predict(task), "has missing values")
 })
