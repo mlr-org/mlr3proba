f7c4156bc46f1ddf0d16f9f22e8ef446470bdf89
john <bblodfon@gmail.com>
2025-06-25 18:58:16 +0200

    add helper autotest function for CR learners

--- inst/testthat/helper_autotest.R
+++ inst/testthat/helper_autotest.R
@@ -26,7 +26,6 @@ sanity_check.PredictionDens = function(prediction, ...) { # nolint
 registerS3method("sanity_check", "PredictionDens", sanity_check.PredictionDens)
 
 generate_tasks.LearnerSurv = function(learner, N = 20L, ...) { # nolint
-
   real_time = round(1 + rexp(N, rate = 2) * 20, 1)
   cens_time = round(1 + rexp(N, rate = 3) * 20, 1)
   status = ifelse(real_time <= cens_time, 1L, 0L)
@@ -53,9 +52,47 @@ generate_tasks.LearnerSurv = function(learner, N = 20L, ...) { # nolint
 }
 registerS3method("generate_tasks", "LearnerSurv", generate_tasks.LearnerSurv)
 
-sanity_check.PredictionSurv = function(prediction, ...) { # nolint
+sanity_check.PredictionSurv = function(prediction, ...) {
   # sanity check discrimination
   prediction$score() >= 0.6
 }
-
 registerS3method("sanity_check", "PredictionSurv", sanity_check.PredictionSurv)
+
+generate_tasks.LearnerCompRisks = function(learner, N = 20L, ...) {
+  times = stats::rexp(N, rate = 0.2) # exp distr for event times
+  # two competing events (0 => censored (30%), 1,2 => events (35% each))
+  event = sample(0:2, size = N, replace = TRUE, prob = c(0.3, 0.35, 0.35))
+
+  data = cbind(data.table::data.table(time = times, event = event), generate_data(learner, N))
+  task = mlr3proba::TaskCompRisks$new(id = "proto", backend = mlr3::as_data_backend(data))
+  tasks = generate_generic_tasks(learner, task)
+
+  # Ensure N is even
+  if (N %% 2 == 1L) N = N + 1L
+  N_half = N / 2L
+
+  # Generate unique times
+  times_group0 = seq(1, N_half)
+  times_group1 = seq(max(times_group0) + 1, max(times_group0) + N_half)
+
+  # Events
+  event_group0 = rep(1L, N_half)
+  event_group1 = sample(c(2L, 0L), size = N_half, replace = TRUE, prob = c(0.7, 0.3))
+
+  data = data.table::data.table(
+    x = rep(0:1, each = N_half),
+    time = c(times_group0, times_group1),
+    event = c(event_group0, event_group1)
+  )
+  tasks$sanity = mlr3proba::TaskCompRisks$new("sanity", mlr3::as_data_backend(data),
+                                              time = "time", event = "event")
+
+  tasks
+}
+registerS3method("generate_tasks", "LearnerCompRisks", generate_tasks.LearnerCompRisks)
+
+sanity_check.PredictionCompRisks = function(prediction, ...) {
+  # sanity check discrimination
+  prediction$score() >= 0.5
+}
+registerS3method("sanity_check", "PredictionCompRisks", sanity_check.PredictionCompRisks)
