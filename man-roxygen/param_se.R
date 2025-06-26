f50d71e9df80451f1fab549f460a2119c35232a1
john <bblodfon@gmail.com>
2025-06-26 16:20:51 +0200

    remove unused file

--- man-roxygen/param_se.R
+++
@@ -1,6 +0,0 @@
-#' @section Parameter details:
-#' - `se` (`logical(1)`)\cr
-#' If `TRUE` then returns standard error of the measure otherwise
-#' returns the mean across all individual scores, e.g. the mean of
-#' the per observation scores.
-#' Default is `FALSE` (returns the mean).
