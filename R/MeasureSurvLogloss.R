c28d69ef7f823569be9f338498ed2b1f0d91eee2
john <bblodfon@gmail.com>
2025-06-27 20:08:17 +0200

    use linear interpolation for pdf in surv.logloss

--- R/MeasureSurvLogloss.R
+++ R/MeasureSurvLogloss.R
@@ -1,31 +1,29 @@
 #' @template surv_measure
 #' @templateVar title Negative Log-Likelihood
 #' @templateVar fullname MeasureSurvLogloss
-#' @templateVar eps 1e-15
+#' @templateVar eps 1e-6
 #' @template param_eps
 #' @template param_erv
 #'
 #' @description
-#' Calculates the cross-entropy, or negative log-likelihood (NLL) or logarithmic (log), loss.
-#' @section Parameter details:
-#' - `IPCW` (`logical(1)`)\cr
-#' If `TRUE` (default) then returns the \eqn{L_{RNLL}} score (which is proper), otherwise the \eqn{L_{NLL}} score (improper). See Sonabend et al. (2024) for more details.
+#' Calculates the cross-entropy, or negative log-likelihood (NLL) or logarithmic (log) loss.
 #'
 #' @details
-#' The Log Loss, in the context of probabilistic predictions, is defined as the
-#' negative log probability density function, \eqn{f}, evaluated at the
-#' observation time (event or censoring), \eqn{t},
-#' \deqn{L_{NLL}(f, t) = -\log[f(t)]}
+#' The (observation-wise) Log-Likelihood is defined as the negative logarithm of
+#' the predicted probability density function \eqn{f_i}, evaluated at the
+#' observation time \eqn{t_i} (event or censoring):
 #'
-#' The **Re-weighted Negative Log-Likelihood** (RNLL) or IPCW (Inverse Probability Censoring Weighted) Log Loss is defined by
-#' \deqn{L_{RNLL}(f, t, \delta) = - \frac{\delta \log[f(t)]}{G(t)}}
-#' where \eqn{\delta} is the censoring indicator and \eqn{G(t)} is the Kaplan-Meier estimator of the
-#' censoring distribution.
-#' So only observations that have experienced the event are taking into account
-#' for RNLL (i.e. \eqn{\delta = 1}) and both \eqn{f(t), G(t)} are calculated only at the event times.
-#' If only censored observations exist in the test set, `NaN` is returned.
+#' \deqn{L_{NLL}(S_i,t_i) = -\log{[f_i(t_i)]}}
 #'
-#' @template details_trainG
+#' This loss does not take into account the censoring status of an observation,
+#' treating all outcomes as events, see Sonabend et al. (2024).
+#'
+#' To get a single score across all \eqn{N} observations of the test set, we
+#' return the average of the observation-wise scores:
+#'
+#' \deqn{\sum_{i=1}^N L_{NLL}(S_i, t_i) / N}
+#'
+#' @template details_interp
 #'
 #' @references
 #' `r format_bib("sonabend_2024")`
@@ -45,11 +43,10 @@ MeasureSurvLogloss = R6Class("MeasureSurvLogloss",
       assert_logical(ERV)
 
       ps = ps(
-        eps = p_dbl(0, 1, default = 1e-15),
-        IPCW = p_lgl(default = TRUE),
+        eps = p_dbl(0, 1, default = 1e-6),
         ERV = p_lgl(default = FALSE)
       )
-      ps$set_values(eps = 1e-15, IPCW = TRUE, ERV = ERV)
+      ps$set_values(eps = 1e-6, ERV = ERV)
 
       range = if (ERV) c(-Inf, 1) else c(0, Inf)
 
@@ -58,8 +55,7 @@ MeasureSurvLogloss = R6Class("MeasureSurvLogloss",
         range = range,
         minimize = !ERV,
         predict_type = "distr",
-        packages = "distr6",
-        label = "Log Loss",
+        label = "Negative Log-Likelihood",
         man = "mlr3proba::mlr_measures_surv.logloss",
         param_set = ps
       )
@@ -70,21 +66,32 @@ MeasureSurvLogloss = R6Class("MeasureSurvLogloss",
 
   private = list(
     .score = function(prediction, task, train_set, ...) {
-      if (self$param_set$values$ERV) {
+      pv = self$param_set$values
+
+      if (pv$ERV) {
         return(.scoring_rule_erv(self, prediction, task, train_set))
       }
-      x = as.integer(!is.null(task)) + as.integer(!is.null(train_set))
-      if (x == 1) {
-        stop("Either 'task' and 'train_set' should be passed to measure or neither.")
-      } else if (x) {
-        train = task$truth(train_set)
-      } else {
-        train = NULL
-      }
 
-      ps = self$param_set$values
+      truth = prediction$truth
+      n_obs = length(truth)
+      test_times = truth[, 1L]
 
-      mean(surv_logloss(prediction$truth, prediction$data$distr, ps$eps, ps$IPCW, train))
+      # get survival matrix
+      surv_mat = .get_surv_matrix(prediction)
+      pred_times = as.numeric(colnames(surv_mat))
+
+      res = vapply(seq_len(n_obs), function(obs_index) {
+        # event time or censoring time
+        outcome_time = test_times[obs_index]
+
+        # predicted survival curve for observation
+        surv_pred = list(surv = surv_mat[obs_index, ], time = pred_times)
+
+        # predicted pdf at observed time
+        .interp_pdf(surv_pred, outcome_time)
+      }, numeric(1))
+
+      mean(-log(pmax(pv$eps, res)))
     }
   )
 )
