cf5fa2b59329b68ac9281cbfc4a766ae77f1997f
john <bblodfon@gmail.com>
2025-06-27 17:06:18 +0200

    add interpolation functions

--- R/helper_measures.R
+++ R/helper_measures.R
@@ -1,5 +1,97 @@
 # helper functions specific for survival measures
 
+# SURVIVAL ----
+# S(t)/f(t) ESTIMATION/INTERPOLATION FUNCTIONS
+
+# Linearly interpolate (and extrapolate) a survival curve
+# @param fit `survfit` object or a `list` with 2 elemnts:
+# `surv` (survival probabilities) and corresponding `time` (time points)
+# @param new_times vector of times (unordered, possibly duplicated)
+# @param inter_type type of interpolation to use - `linear` (default) or `constant`
+# @return interpolated S(t) values
+.interp_surv = function(fit, new_times, inter_type = "linear") {
+  assert_choice(inter_type, c("linear", "constant"))
+
+  # constant interpolation is easy
+  if (inter_type == "constant") {
+    surv = fit$surv
+    times = fit$time
+
+    return(stats::approx(x = times, y = surv, xout = new_times, yleft = 1,
+                         method = "constant", rule = 2)$y)
+  }
+
+  # remove constant-interpolated values from S(t)
+  keep = !duplicated(fit$surv)
+  surv = fit$surv[keep] # decreasing
+  times = fit$time[keep] # ordered
+
+  # Edge case: constant survival
+  if (length(unique(surv)) == 1) {
+    return(rep(surv[1], length(new_times)))
+  }
+
+  # linear interpolation (at least two S(t) values here)
+  int_surv = stats::approx(x = times, y = surv, xout = new_times,
+                           yleft = 1, method = "linear", rule = 2)$y
+
+  # Extrapolate manually if needed
+  min_time = min(times)
+  max_time = max(times)
+
+  # Precompute slopes for extrapolation
+  slope_min = (surv[1L] - 1) / min_time
+  slope_max = (surv[length(surv)] - surv[length(surv) - 1L]) / (max_time - times[length(times) - 1L])
+
+  idx_min = new_times < min_time
+  idx_max = new_times > max_time
+
+  # Linear extrapolation considering that S(t = 0) = 1
+  if (any(idx_min) && surv[1L] < 1) {
+    int_surv[idx_min] = 1 + slope_min * new_times[idx_min]
+  }
+
+  # Linear extrapolation using the last time interval
+  if (any(idx_max) && surv[length(surv)] > 0) {
+    extrapolated_value = surv[length(surv)] + slope_max * (new_times[idx_max] - max_time)
+    int_surv[idx_max] = pmax(0, extrapolated_value) # force S >= 0
+  }
+
+  int_surv
+}
+
+# PDF estimation from a survival curve
+# @param fit `survfit` object or a `list` with 2 elemnts:
+# `surv` (survival probabilities) and corresponding `time` (time points)
+# @param new_times numeric vector (unordered, duplicated allowed)
+# @return numeric vector of density values f(t)
+.interp_pdf = function(fit, new_times) {
+  # keep all unique sorted times (predicted and requested) for pdf
+  utimes = sort(unique(c(fit$time, new_times)))
+
+  # Create a mapping of `new_times` to `utimes`
+  indx = match(new_times, utimes)
+
+  # Linearly interpolate survival function (to avoid pdf = 0 problems)
+  surv = .interp_surv(fit, utimes, inter_type = "linear")
+
+  # CDF = 1 - S
+  cdf = 1 - surv
+
+  # Numerical derivative: f = dF/dt
+  dt = diff(utimes)
+  dF = diff(cdf)
+
+  # Density (finite difference)
+  dens = dF / dt
+
+  # For timepoints exactly at utimes, align left
+  dens_full = c(dens[1], dens) # replicate first slope for first point
+
+  # return density at `new_times`, clip any negatives to 0
+  pmax(dens_full[indx], 0)
+}
+
 # Compute per-observation prediction errors for uncensored survival times.
 #
 # This function returns either squared or absolute differences between predicted
@@ -42,10 +134,13 @@
 
   measure$param_set$set_values(ERV = TRUE)
 
-  # return percentage decrease
+  # return R^2-like score
+  # 0 => same as base score, >0 => better than baseline
+  # <0 => worse than baseline
   1 - (learner_score / base_score)
 }
 
+# COMPETING RISKS ----
 ## constant interpolate CIF matrix to requested `new_times`
 .interp_cif = function(cif_mat, new_times) {
   # predicted time points
