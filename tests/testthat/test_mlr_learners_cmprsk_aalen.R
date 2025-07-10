66a6c9df569ca8021572aaa6d2e3f28075b6ecb7
john <bblodfon@gmail.com>
2025-07-10 12:25:46 +0200

    increase N for AJ autotest

--- tests/testthat/test_mlr_learners_cmprsk_aalen.R
+++ tests/testthat/test_mlr_learners_cmprsk_aalen.R
@@ -2,7 +2,7 @@ test_that("autotest", {
   with_seed(42L, {
     learner = lrn("cmprsk.aalen")
     expect_learner(learner)
-    result = run_autotest(learner, check_replicable = FALSE)
+    result = run_autotest(learner, N = 42, check_replicable = FALSE)
     expect_true(result, info = result$error)
   })
 })
