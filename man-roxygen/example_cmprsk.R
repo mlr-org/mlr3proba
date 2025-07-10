f1e450b74f8641f1f515274521c12406c2c5b5ca
john <bblodfon@gmail.com>
2025-07-10 12:26:36 +0200

    improve example template

--- man-roxygen/example_cmprsk.R
+++ man-roxygen/example_cmprsk.R
@@ -24,11 +24,13 @@
 #'
 #' # Score the predictions
 #' <%= if(msr_id == "auc" || msr_id == "all") "
-#' ## AUC(t = 100), 1st cause
-#' predictions$score(msr(\"cmprsk.auc\", cause = 1, time_horizon = 100))
-#' ## AUC(t = 100), 2nd cause
-#' predictions$score(msr(\"cmprsk.auc\", cause = 2, time_horizon = 100))
-#' ## AUC(t = 100), summary score across causes
+#' # AUC(t = 100), weighted mean score across causes (default)
 #' predictions$score(msr(\"cmprsk.auc\", cause = \"mean\", time_horizon = 100))
+#'
+#' # AUC(t = 100), 1st cause
+#' predictions$score(msr(\"cmprsk.auc\", cause = 1, time_horizon = 100))
+#'
+#' # AUC(t = 100), 2nd cause
+#' predictions$score(msr(\"cmprsk.auc\", cause = 2, time_horizon = 100))
 #' " %>
 #'
